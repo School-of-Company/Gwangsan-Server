e824eae880d89bd677d6b120f5a3eda8404f41c2
김승찬 <s24041@gsm.hs.kr>
2025-05-12 22:40:59 +0900

    init

---
+++ src/main/java/team/startup/gwangsan/GwangsanApplication.java
@@ -0,0 +1,13 @@
+package team.startup.gwangsan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GwangsanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GwangsanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/team/startup/gwangsan/GwangsanApplicationTests.java
@@ -0,0 +1,13 @@
+package team.startup.gwangsan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GwangsanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
