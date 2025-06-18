6e209af6a4c0ed54e5e82f6df25b6a0b87c764e1
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-06-18 20:35:29 +0900

    add :: 채팅 메세지 엔티티 추가

---
+++ src/main/java/team/startup/gwangsan/chat/entity/ChatMessage.java
@@ -0,0 +1,49 @@
+package team.startup.gwangsan.chat.entity;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import team.startup.gwangsan.member.entity.Member;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "tbl_chat_message")
+public class ChatMessage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "message_id")
+    private Long id;
+
+    @Column(name = "content", nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @CreatedDate
+    @Column(name = "timestamp", nullable = false)
+    private LocalDateTime timestamp;
+
+    @Column(name = "checked", nullable = false)
+    private Boolean checked;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id", nullable = false)
+    private ChatRoom room;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sender_id", nullable = false)
+    private Member sender;
+
+    @Builder
+    public ChatMessage(String content, LocalDateTime timestamp, Boolean checked, ChatRoom room, Member sender) {
+        this.content = content;
+        this.timestamp = timestamp;
+        this.checked = checked;
+        this.room = room;
+        this.sender = sender;
+    }
+}
