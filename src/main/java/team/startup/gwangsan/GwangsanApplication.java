97f52a7048cccde4bbcb5f22363d343b6bed33b2
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-06-18 20:36:01 +0900

    add :: EnableJpaRepositories 어노테이션 추가

--- src/main/java/team/startup/gwangsan/GwangsanApplication.java
+++ src/main/java/team/startup/gwangsan/GwangsanApplication.java
@@ -2,8 +2,10 @@ package team.startup.gwangsan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class GwangsanApplication {
 
 	public static void main(String[] args) {
