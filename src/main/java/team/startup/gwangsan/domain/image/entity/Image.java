836369b0ea3cc1d901a8765d09bf6afebd75bea9
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-06-25 23:10:45 +0900

    update :: 공통 Image 엔티티로 변경

---
+++ src/main/java/team/startup/gwangsan/domain/image/entity/Image.java
@@ -0,0 +1,26 @@
+package team.startup.gwangsan.domain.image.entity;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "tbl_image")
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "image_id")
+    private Long id;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @Builder
+    public Image(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
