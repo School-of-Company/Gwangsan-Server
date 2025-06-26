664f9f272349a481491b54f454ea697ad876a1fa
김승찬 <s24041@gsm.hs.kr>
2025-06-26 23:13:49 +0900

    add :: errorresponse 추가

---
+++ src/main/java/team/startup/gwangsan/global/exception/ErrorResponse.java
@@ -0,0 +1,13 @@
+package team.startup.gwangsan.global.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class ErrorResponse {
+    private final int status;
+    private final String message;
+}
