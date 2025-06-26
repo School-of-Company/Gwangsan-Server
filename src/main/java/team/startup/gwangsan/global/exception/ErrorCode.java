b05d9937ee0a77e232a9f186fcf0e454720f9c34
김승찬 <s24041@gsm.hs.kr>
2025-06-26 23:13:33 +0900

    add :: member관련 errorcode 추가

--- src/main/java/team/startup/gwangsan/global/exception/ErrorCode.java
+++ src/main/java/team/startup/gwangsan/global/exception/ErrorCode.java
@@ -12,6 +12,7 @@ public enum ErrorCode {
     INVALID_TOKEN(401, "유효하지 않은 토큰입니다."),
 
     // member
+    INVALID_MEMBER_PRINCIPAL(401, "현재 인증된 사용자의 정보가 유효하지 않습니다."),
     NOT_FOUND_MEMBER(404, "해당 회원을 찾을 수 없습니다."),
 
     // server
