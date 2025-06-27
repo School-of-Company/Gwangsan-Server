6c7cb68048937daa36715101dc5693f806156297
김승찬 <s24041@gsm.hs.kr>
2025-06-28 00:58:46 +0900

    add :: SmsAuthEntity 추가

---
+++ src/main/java/team/startup/gwangsan/domain/sms/entity/SmsAuthEntity.java
@@ -0,0 +1,28 @@
+package team.startup.gwangsan.domain.sms.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.index.Indexed;
+
+@RedisHash(value = "phone_authentication", timeToLive = 60L * 3)
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SmsAuthEntity {
+
+    @Id
+    private String phone;
+
+    @Indexed
+    private String randomValue;
+
+    private Boolean authentication;
+
+    private Integer attemptCount;
+}
+
