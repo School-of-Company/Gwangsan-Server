cd2657796f7a8230caddeb827c9d0c31ecfacffb
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-06-27 20:25:03 +0900

    add :: findByIdIn 메서드로 변경

--- src/main/java/team/startup/gwangsan/domain/image/repository/ImageRepository.java
+++ src/main/java/team/startup/gwangsan/domain/image/repository/ImageRepository.java
@@ -1,12 +1,14 @@
 package team.startup.gwangsan.domain.image.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import team.startup.gwangsan.domain.image.entity.Image;
 
 import java.util.List;
 
-public interface ImageRepository extends CrudRepository<Image, Long> {
-    List<Image> findByImageUrlIn(List<String> imageUrls);
+public interface ImageRepository extends JpaRepository<Image, Long> {
+    List<Image> findByIdIn(List<Long> imageIds);
 
     List<Image> findImagesById(Long id);
+
+
 }
