e943c8567d423080ae9bf0cd4169154346dbcaf5
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-06-27 09:14:00 +0900

    add :: 비동기 처리를 위한 asyncConfig 추가

---
+++ src/main/java/team/startup/gwangsan/global/config/AsyncConfig.java
@@ -0,0 +1,10 @@
+package team.startup.gwangsan.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+}
