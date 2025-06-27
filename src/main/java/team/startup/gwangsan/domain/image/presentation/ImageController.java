1ba943c8156ed95e8507307e9a0655371234500a
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-06-27 20:31:57 +0900

    add :: ImageController 추가

---
+++ src/main/java/team/startup/gwangsan/domain/image/presentation/ImageController.java
@@ -0,0 +1,32 @@
+package team.startup.gwangsan.domain.image.presentation;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import team.startup.gwangsan.domain.image.presentation.dto.response.UploadImageResponse;
+import team.startup.gwangsan.domain.image.service.DeleteImageService;
+import team.startup.gwangsan.domain.image.service.UploadImageService;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/image")
+public class ImageController {
+
+    private final UploadImageService imageUploadService;
+    private final DeleteImageService imageDeleteService;
+
+    @PostMapping
+    public ResponseEntity<UploadImageResponse> upload(@RequestBody @Valid MultipartFile file) {
+        UploadImageResponse response = imageUploadService.execute(file);
+        return ResponseEntity.ok().body(response);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        imageDeleteService.execute(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
