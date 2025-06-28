2d3bf14aae996cc0963df539db62c456c099fac4
김승찬 <s24041@gsm.hs.kr>
2025-06-29 00:42:02 +0900

    add :: SmsController 추가

---
+++ src/main/java/team/startup/gwangsan/domain/sms/presentation/SmsController.java
@@ -0,0 +1,25 @@
+package team.startup.gwangsan.domain.sms.presentation;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import team.startup.gwangsan.domain.sms.presentation.dto.SendSmsRequest;
+import team.startup.gwangsan.domain.sms.service.SendSmsService;
+
+@RestController
+@RequestMapping("/sms")
+@RequiredArgsConstructor
+public class SmsController {
+
+    private final SendSmsService sendSmsService;
+
+    @PostMapping
+    public ResponseEntity<Void> sendSms(@RequestBody @Valid SendSmsRequest request) {
+        sendSmsService.execute(request);
+        return ResponseEntity.ok().build();
+    }
+}
