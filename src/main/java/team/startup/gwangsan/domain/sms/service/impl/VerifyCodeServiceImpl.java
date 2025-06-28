74d1be4c8715ab5dd0cc3a18b666611cd53f7d76
김승찬 <s24041@gsm.hs.kr>
2025-06-29 01:57:31 +0900

    add :: VerifyCodeServiceImpl 추가

---
+++ src/main/java/team/startup/gwangsan/domain/sms/service/impl/VerifyCodeServiceImpl.java
@@ -0,0 +1,32 @@
+package team.startup.gwangsan.domain.sms.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import team.startup.gwangsan.domain.auth.exception.SmsAuthNotFoundException;
+import team.startup.gwangsan.domain.sms.entity.SmsAuthEntity;
+import team.startup.gwangsan.domain.sms.exception.NotMatchRandomCodeException;
+import team.startup.gwangsan.domain.sms.presentation.dto.VerifyCodeRequest;
+import team.startup.gwangsan.domain.sms.repository.SmsAuthRepository;
+import team.startup.gwangsan.domain.sms.service.VerifyCodeService;
+
+@Service
+@RequiredArgsConstructor
+public class VerifyCodeServiceImpl implements VerifyCodeService {
+
+    private final SmsAuthRepository smsAuthRepository;
+
+    @Override
+    @Transactional
+    public void execute(VerifyCodeRequest request) {
+        SmsAuthEntity smsAuthEntity = smsAuthRepository.findById(request.phoneNumber())
+                .orElseThrow(SmsAuthNotFoundException::new);
+
+        if (!smsAuthEntity.getRandomValue().equals(request.code())) {
+            throw new NotMatchRandomCodeException();
+        }
+
+        smsAuthEntity.changeAuthentication();
+        smsAuthRepository.save(smsAuthEntity);
+    }
+}
