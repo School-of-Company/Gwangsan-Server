01a440b672622d9f5e1f8689d18587b17ae21c94
김승찬 <s24041@gsm.hs.kr>
2025-06-29 22:10:00 +0900

    update :: 패키지 추가에 따른 경로 변경

--- src/main/java/team/startup/gwangsan/domain/auth/service/SignUpService.java
+++ src/main/java/team/startup/gwangsan/domain/auth/service/SignUpService.java
@@ -1,6 +1,6 @@
 package team.startup.gwangsan.domain.auth.service;
 
-import team.startup.gwangsan.domain.auth.presentation.dto.SignUpRequest;
+import team.startup.gwangsan.domain.auth.presentation.dto.request.SignUpRequest;
 
 public interface SignUpService {
     void execute(SignUpRequest request);
