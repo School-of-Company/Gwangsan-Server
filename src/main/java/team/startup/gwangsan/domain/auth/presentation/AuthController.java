8fa7a935e44a3cd933d659414bf1715784956a3b
김승찬 <s24041@gsm.hs.kr>
2025-06-29 22:10:28 +0900

    add :: signin controller 추가

--- src/main/java/team/startup/gwangsan/domain/auth/presentation/AuthController.java
+++ src/main/java/team/startup/gwangsan/domain/auth/presentation/AuthController.java
@@ -8,7 +8,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import team.startup.gwangsan.domain.auth.presentation.dto.SignUpRequest;
+import team.startup.gwangsan.domain.auth.presentation.dto.request.SignInRequest;
+import team.startup.gwangsan.domain.auth.presentation.dto.request.SignUpRequest;
+import team.startup.gwangsan.domain.auth.presentation.dto.response.TokenResponse;
+import team.startup.gwangsan.domain.auth.service.SignInService;
 import team.startup.gwangsan.domain.auth.service.SignUpService;
 
 @RestController
@@ -17,11 +20,18 @@ import team.startup.gwangsan.domain.auth.service.SignUpService;
 public class AuthController {
 
     private final SignUpService signUpService;
+    private final SignInService signInService;
 
     @PostMapping("/signup")
     public ResponseEntity<Void> signUp(@RequestBody @Valid SignUpRequest request) {
         signUpService.execute(request);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<TokenResponse> signIn(@RequestBody @Valid SignInRequest request) {
+        TokenResponse response = signInService.execute(request);
+        return ResponseEntity.ok(response);
     }
 }
 
