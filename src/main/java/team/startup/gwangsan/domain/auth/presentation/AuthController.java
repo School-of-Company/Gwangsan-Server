2f25cfe25d188f1a8eafdbd29a8c977ffb215c41
김승찬 <s24041@gsm.hs.kr>
2025-06-29 23:15:14 +0900

    add :: reissue를 controller에 추가

--- src/main/java/team/startup/gwangsan/domain/auth/presentation/AuthController.java
+++ src/main/java/team/startup/gwangsan/domain/auth/presentation/AuthController.java
@@ -4,13 +4,11 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import team.startup.gwangsan.domain.auth.presentation.dto.request.SignInRequest;
 import team.startup.gwangsan.domain.auth.presentation.dto.request.SignUpRequest;
 import team.startup.gwangsan.domain.auth.presentation.dto.response.TokenResponse;
+import team.startup.gwangsan.domain.auth.service.ReissueTokenService;
 import team.startup.gwangsan.domain.auth.service.SignInService;
 import org.springframework.web.bind.annotation.*;
 import team.startup.gwangsan.domain.auth.presentation.dto.request.SignInRequest;
@@ -29,18 +27,13 @@ public class AuthController {
     private final SignUpService signUpService;
     private final SignInService signInService;
     private final ReissueTokenService reissueTokenService;
+    private final ReissueTokenService reissueTokenService;
     private final SignOutService signOutService;
 
     @PostMapping("/signup")
     public ResponseEntity<Void> signUp(@RequestBody @Valid SignUpRequest request) {
         signUpService.execute(request);
         return ResponseEntity.status(HttpStatus.CREATED).build();
-    }
-
-    @PostMapping("/signin")
-    public ResponseEntity<TokenResponse> signIn(@RequestBody @Valid SignInRequest request) {
-        TokenResponse response = signInService.execute(request);
-        return ResponseEntity.ok(response);
     }
 
     @PostMapping("/signin")
@@ -59,6 +52,12 @@ public class AuthController {
     public ResponseEntity<Void> signout() {
         signOutService.execute();
         return ResponseEntity.ok().build();
+    }
+
+    @PatchMapping("/reissue")
+    public ResponseEntity<TokenResponse> reissueToken(@RequestHeader("RefreshToken") String refreshToken) {
+        TokenResponse tokenResponse = reissueTokenService.execute(refreshToken);
+        return ResponseEntity.ok(tokenResponse);
     }
 }
 
