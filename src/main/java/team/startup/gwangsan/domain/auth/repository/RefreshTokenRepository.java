a678dd09e393520bc50ec3acca8fbbebf2b3f8f6
김승찬 <s24041@gsm.hs.kr>
2025-06-29 22:12:13 +0900

    add :: RefreshTokenRepository 추가

---
+++ src/main/java/team/startup/gwangsan/domain/auth/repository/RefreshTokenRepository.java
@@ -0,0 +1,7 @@
+package team.startup.gwangsan.domain.auth.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import team.startup.gwangsan.domain.auth.entity.RefreshToken;
+
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
+}
