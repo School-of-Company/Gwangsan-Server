8aea2ef82182db13ae2d67fd7c60a2bf18bcced7
김승찬 <s24041@gsm.hs.kr>
2025-06-30 20:01:15 +0900

    update :: 패키지 구조 변경

--- src/main/java/team/startup/gwangsan/domain/auth/repository/DeviceTokenRepository.java
+++
@@ -1,7 +0,0 @@
-package team.startup.gwangsan.domain.auth.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import team.startup.gwangsan.domain.auth.entity.DeviceToken;
-
-public interface DeviceTokenRepository extends CrudRepository<DeviceToken, String> {
-}
