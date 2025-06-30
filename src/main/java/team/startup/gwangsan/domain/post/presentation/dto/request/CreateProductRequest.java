37d22af0be525abb1a166e8d8593b4a48524eaaa
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-06-30 22:37:01 +0900

    add :: CreateProductRequest

---
+++ src/main/java/team/startup/gwangsan/domain/post/presentation/dto/request/CreateProductRequest.java
@@ -0,0 +1,18 @@
+package team.startup.gwangsan.domain.post.presentation.dto.request;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotNull;
+import team.startup.gwangsan.domain.post.entity.constant.Mode;
+import team.startup.gwangsan.domain.post.entity.constant.Type;
+
+import java.util.List;
+
+public record CreateProductRequest(
+        @NotNull Type type,
+        @NotNull Mode mode,
+        @NotNull @Max(20) String title,
+        @NotNull String description,
+        @NotNull Integer gwangsan,
+        @NotNull List<Long> imageIds
+) {
+}
