23cb32bc4b003deabb4b2bd949ec5f7168548605
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-06-30 22:34:51 +0900

    add :: ProductImageCustomRepository 구현

---
+++ src/main/java/team/startup/gwangsan/domain/post/repository/custom/impl/ProductImageCustomRepositoryImpl.java
@@ -0,0 +1,29 @@
+package team.startup.gwangsan.domain.post.repository.custom.impl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import team.startup.gwangsan.domain.post.entity.ProductImage;
+import team.startup.gwangsan.domain.post.repository.custom.ProductImageCustomRepository;
+
+import java.util.List;
+
+import static team.startup.gwangsan.domain.post.entity.QProductImage.productImage;
+
+@Repository
+@RequiredArgsConstructor
+public class ProductImageCustomRepositoryImpl implements ProductImageCustomRepository {
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public List<ProductImage> findProductImageByProductIdIn(List<Long> productIds) {
+        return queryFactory
+                .selectFrom(productImage).distinct()
+                .join(productImage.image).fetchJoin()
+                .where(
+                        productImage.product.id.in(productIds)
+                )
+                .fetch();
+    }
+}
