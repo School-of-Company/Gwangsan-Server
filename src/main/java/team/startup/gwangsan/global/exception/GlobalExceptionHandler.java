a79cc2cd099c3f1c594f67daa457d1c9c5452b41
김승찬 <s24041@gsm.hs.kr>
2025-07-01 15:13:46 +0900

    add :: 500error GlobalExceptionHandler 추가

--- src/main/java/team/startup/gwangsan/global/exception/GlobalExceptionHandler.java
+++ src/main/java/team/startup/gwangsan/global/exception/GlobalExceptionHandler.java
@@ -1,9 +1,12 @@
 package team.startup.gwangsan.global.exception;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -15,5 +18,12 @@ public class GlobalExceptionHandler {
                 .build();
 
         return ResponseEntity.status(response.getStatus()).body(response);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception ex) {
+        log.error("Unhandled Exception: ", ex);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ErrorResponse(500, ex.getMessage()));
     }
 }
