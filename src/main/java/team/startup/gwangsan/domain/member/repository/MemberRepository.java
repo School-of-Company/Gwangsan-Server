0d9b922568dc993ab36bb5aa861935be5928cd55
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-07-02 18:21:48 +0900

    update :: JPA 메서드 추가

--- src/main/java/team/startup/gwangsan/domain/member/repository/MemberRepository.java
+++ src/main/java/team/startup/gwangsan/domain/member/repository/MemberRepository.java
@@ -2,7 +2,9 @@ package team.startup.gwangsan.domain.member.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import team.startup.gwangsan.domain.member.entity.Member;
+import team.startup.gwangsan.domain.member.entity.constant.MemberStatus;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
@@ -13,4 +15,8 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     boolean existsByNickname(String nickname);
 
     Optional<Member> findByNickname(String nickname);
+
+    Optional<Member> findByStatusAndId(MemberStatus status, Long id);
+
+    List<Member> findAllByStatus(MemberStatus status);
 }
