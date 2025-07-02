b71cfb9aae21ba30349fcb8ad35dddfda663ee0f
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-07-02 19:58:08 +0900

    add :: FindAlertByAlertTypeAndPlaceService 추가

---
+++ src/main/java/team/startup/gwangsan/domain/admin/service/FindAlertByAlertTypeAndPlaceService.java
@@ -0,0 +1,8 @@
+package team.startup.gwangsan.domain.admin.service;
+
+import team.startup.gwangsan.domain.admin.entity.constant.AlertType;
+import team.startup.gwangsan.domain.admin.presentation.dto.response.GetAdminAlertResponse;
+
+public interface FindAlertByAlertTypeAndPlaceService {
+    GetAdminAlertResponse execute(String placeName, AlertType type);
+}
