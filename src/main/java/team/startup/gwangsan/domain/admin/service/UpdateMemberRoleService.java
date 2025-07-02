7cf436f3978eb4e0aece7d8361d77fe9a31629c1
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-07-02 18:14:46 +0900

    add :: UpdateMemberRoleService 추가

---
+++ src/main/java/team/startup/gwangsan/domain/admin/service/UpdateMemberRoleService.java
@@ -0,0 +1,7 @@
+package team.startup.gwangsan.domain.admin.service;
+
+import team.startup.gwangsan.domain.member.entity.constant.MemberRole;
+
+public interface UpdateMemberRoleService {
+    void execute(Long memberId, MemberRole role);
+}
