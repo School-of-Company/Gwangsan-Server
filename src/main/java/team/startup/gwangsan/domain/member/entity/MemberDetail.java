0868cac63d576931008eb7ad6b61b819157a7cb9
김승찬 <s24041@gsm.hs.kr>
2025-07-03 01:40:35 +0900

    add :: 내 정보 수집 api 추가

--- src/main/java/team/startup/gwangsan/domain/member/entity/MemberDetail.java
+++ src/main/java/team/startup/gwangsan/domain/member/entity/MemberDetail.java
@@ -56,4 +56,8 @@ public class MemberDetail {
         this.profileUrl = profileUrl;
     }
 
+
+    public void updateProfileUrl(String profileUrl) {
+        this.profileUrl = profileUrl;
+    }
 }
