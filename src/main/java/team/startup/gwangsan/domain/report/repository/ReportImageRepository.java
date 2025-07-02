29a53581fea062dabb3d445ff167747a48ccfdfc
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-07-02 20:16:19 +0900

    update :: findByReportIn 메서드 추가

--- src/main/java/team/startup/gwangsan/domain/report/repository/ReportImageRepository.java
+++ src/main/java/team/startup/gwangsan/domain/report/repository/ReportImageRepository.java
@@ -1,8 +1,14 @@
 package team.startup.gwangsan.domain.report.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import team.startup.gwangsan.domain.report.entity.Report;
 import team.startup.gwangsan.domain.report.entity.ReportImage;
+
+import java.util.Collection;
+import java.util.List;
 
 public interface ReportImageRepository extends JpaRepository<ReportImage, Long> {
     void deleteByReportIdAndImageId(Long reportId, Long imageId);
+
+    List<ReportImage> findByReportIn(Collection<Report> reports);
 }
