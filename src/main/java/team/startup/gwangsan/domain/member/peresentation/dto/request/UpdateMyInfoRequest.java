6fd162a35246cd34c4be294e48e0fd69887c0725
김승찬 <s24041@gsm.hs.kr>
2025-07-03 00:43:06 +0900

    add :: UpdateMyInfoRequest 추가

---
+++ src/main/java/team/startup/gwangsan/domain/member/peresentation/dto/request/UpdateMyInfoRequest.java
@@ -0,0 +1,9 @@
+package team.startup.gwangsan.domain.member.peresentation.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record UpdateMyInfoRequest(
+        @NotBlank String nickname,
+        @NotBlank String profileUrl
+) {}
+
