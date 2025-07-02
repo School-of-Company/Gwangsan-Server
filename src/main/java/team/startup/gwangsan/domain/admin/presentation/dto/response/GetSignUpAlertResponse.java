f036ffa6a04cc5398e39548f20722aca9eeb49a2
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-07-02 17:53:10 +0900

    add :: GetSignUpAlertResponse 추기

---
+++ src/main/java/team/startup/gwangsan/domain/admin/presentation/dto/response/GetSignUpAlertResponse.java
@@ -0,0 +1,12 @@
+package team.startup.gwangsan.domain.admin.presentation.dto.response;
+
+import java.time.LocalDateTime;
+
+public record GetSignUpAlertResponse(
+        Long memberId,
+        String nickname,
+        String title,
+        String recommenderNickname,
+        LocalDateTime createdAt
+) {
+}
