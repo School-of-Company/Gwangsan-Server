f4c6a416b2fe204d5370a4e06a3d958675129eca
김승찬 <s24041@gsm.hs.kr>
2025-07-03 01:41:10 +0900

    add :: 특정 사용자 정보 조회 controller에 추가

--- src/main/java/team/startup/gwangsan/domain/member/peresentation/MemberController.java
+++ src/main/java/team/startup/gwangsan/domain/member/peresentation/MemberController.java
@@ -1,10 +1,15 @@
 package team.startup.gwangsan.domain.member.peresentation;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import team.startup.gwangsan.domain.member.peresentation.dto.request.UpdateMyInfoRequest;
 import team.startup.gwangsan.domain.member.peresentation.dto.response.FindMyInfoResponse;
+import team.startup.gwangsan.domain.member.peresentation.dto.response.FindUserInfoResponse;
 import team.startup.gwangsan.domain.member.service.FindMyInfoService;
+import team.startup.gwangsan.domain.member.service.FindUserInfoService;
+import team.startup.gwangsan.domain.member.service.UpdateMyInfoService;
 
 @RestController
 @RequestMapping("/api/member")
@@ -12,10 +17,24 @@ import team.startup.gwangsan.domain.member.service.FindMyInfoService;
 public class MemberController {
 
     private final FindMyInfoService getMyInfoService;
+    private final UpdateMyInfoService updateMyInfoService;
+    private final FindUserInfoService findUserInfoService;
 
     @GetMapping
     public ResponseEntity<FindMyInfoResponse> findMyInfo() {
         return ResponseEntity.ok(getMyInfoService.execute());
+    }
+
+    @PatchMapping
+    public ResponseEntity<Void> updateMyInfo(@RequestBody @Valid UpdateMyInfoRequest request) {
+        updateMyInfoService.execute(request);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<FindUserInfoResponse> findUserInfo(@PathVariable("id") Long memberId) {
+        FindUserInfoResponse response = findUserInfoService.execute(memberId);
+        return ResponseEntity.ok(response);
     }
 }
 
