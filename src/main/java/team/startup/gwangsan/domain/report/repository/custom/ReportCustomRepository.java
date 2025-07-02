3f0e242636d4dcae16fecdac8d3af707d57270f6
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-07-02 20:14:01 +0900

    add :: ReportCustomRepository 추가

---
+++ src/main/java/team/startup/gwangsan/domain/report/repository/custom/ReportCustomRepository.java
@@ -0,0 +1,10 @@
+package team.startup.gwangsan.domain.report.repository.custom;
+
+import team.startup.gwangsan.domain.place.entity.Place;
+import team.startup.gwangsan.domain.report.entity.Report;
+
+import java.util.List;
+
+public interface ReportCustomRepository {
+    List<Report> findByPlace(Place place);
+}
