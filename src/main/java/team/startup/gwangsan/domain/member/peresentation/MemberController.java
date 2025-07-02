e244f4df9c61a61ceac8f0772c14d4468fc5a214
김승찬 <s24041@gsm.hs.kr>
2025-07-03 00:42:53 +0900

    add :: 내 정보 조회 controller에 추가

--- src/main/java/team/startup/gwangsan/domain/member/peresentation/MemberController.java
+++ src/main/java/team/startup/gwangsan/domain/member/peresentation/MemberController.java
@@ -1,10 +1,13 @@
 package team.startup.gwangsan.domain.member.peresentation;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import team.startup.gwangsan.domain.member.peresentation.dto.request.UpdateMyInfoRequest;
 import team.startup.gwangsan.domain.member.peresentation.dto.response.FindMyInfoResponse;
 import team.startup.gwangsan.domain.member.service.FindMyInfoService;
+import team.startup.gwangsan.domain.member.service.UpdateMyInfoService;
 
 @RestController
 @RequestMapping("/api/member")
@@ -12,10 +15,17 @@ import team.startup.gwangsan.domain.member.service.FindMyInfoService;
 public class MemberController {
 
     private final FindMyInfoService getMyInfoService;
+    private final UpdateMyInfoService updateMyInfoService;
 
     @GetMapping
     public ResponseEntity<FindMyInfoResponse> findMyInfo() {
         return ResponseEntity.ok(getMyInfoService.execute());
+    }
+
+    @PatchMapping
+    public ResponseEntity<Void> updateMyInfo(@RequestBody @Valid UpdateMyInfoRequest request) {
+        updateMyInfoService.execute(request);
+        return ResponseEntity.ok().build();
     }
 }
 
