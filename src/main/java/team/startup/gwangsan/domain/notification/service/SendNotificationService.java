c004ff521fa28a5ea3e6f796969b2e6b0231e364
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-07-04 08:11:31 +0900

    add :: SendNotificationService 추가

---
+++ src/main/java/team/startup/gwangsan/domain/notification/service/SendNotificationService.java
@@ -0,0 +1,9 @@
+package team.startup.gwangsan.domain.notification.service;
+
+import team.startup.gwangsan.domain.notification.entity.constant.NotificationType;
+
+import java.util.List;
+
+public interface SendNotificationService {
+    void execute(List<String> deviceTokens, NotificationType type);
+}
