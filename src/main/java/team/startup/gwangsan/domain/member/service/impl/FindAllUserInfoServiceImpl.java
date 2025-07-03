fef5be1bafe11527d25b2b8305bf1463c2f4e0ef
김승찬 <s24041@gsm.hs.kr>
2025-07-03 17:35:25 +0900

    update :: n+1 문제 해결

--- src/main/java/team/startup/gwangsan/domain/member/service/impl/FindAllUserInfoServiceImpl.java
+++ src/main/java/team/startup/gwangsan/domain/member/service/impl/FindAllUserInfoServiceImpl.java
@@ -3,11 +3,8 @@ package team.startup.gwangsan.domain.member.service.impl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import team.startup.gwangsan.domain.member.entity.MemberDetail;
-import team.startup.gwangsan.domain.member.exception.NotFoundMemberDetailException;
 import team.startup.gwangsan.domain.member.peresentation.dto.response.FindAllUserInfoResponse;
 import team.startup.gwangsan.domain.member.repository.MemberDetailRepository;
-import team.startup.gwangsan.domain.member.repository.MemberRepository;
 import team.startup.gwangsan.domain.member.service.FindAllUserInfoService;
 
 import java.util.List;
@@ -16,20 +13,16 @@ import java.util.List;
 @RequiredArgsConstructor
 public class FindAllUserInfoServiceImpl implements FindAllUserInfoService {
 
-    private final MemberRepository memberRepository;
     private final MemberDetailRepository memberDetailRepository;
 
     @Override
     @Transactional(readOnly = true)
     public List<FindAllUserInfoResponse> execute() {
-
-        return memberRepository.findAll().stream()
-                .map(member -> {
-                    MemberDetail detail = memberDetailRepository.findById(member.getId())
-                            .orElseThrow(NotFoundMemberDetailException::new);
+        return memberDetailRepository.findAllWithMember().stream()
+                .map(detail -> {
                     return new FindAllUserInfoResponse(
-                            member.getId(),
-                            member.getNickname(),
+                            detail.getMember().getId(),
+                            detail.getMember().getNickname(),
                             detail.getProfileUrl(),
                             detail.getLight(),
                             detail.getGwangsan()
@@ -38,4 +31,3 @@ public class FindAllUserInfoServiceImpl implements FindAllUserInfoService {
                 .toList();
     }
 }
-
