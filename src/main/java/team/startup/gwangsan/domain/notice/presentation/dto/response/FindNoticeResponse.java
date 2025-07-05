3135e1ddc99b8471dd480dfe7063c0ff4aa3134c
김승찬 <s24041@gsm.hs.kr>
2025-07-05 23:46:17 +0900

    add :: FindNoticeResponse 추가

---
+++ src/main/java/team/startup/gwangsan/domain/notice/presentation/dto/response/FindNoticeResponse.java
@@ -0,0 +1,17 @@
+package team.startup.gwangsan.domain.notice.presentation.dto.response;
+
+import team.startup.gwangsan.domain.image.presentation.dto.response.GetImageResponse;
+
+import java.util.List;
+
+public record FindNoticeResponse(
+        Long id,
+        String title,
+        String content,
+        String place,
+        String createdAt,
+        String role,
+        List<GetImageResponse> images
+) {
+}
+
