e484c5a5cc31de02da3e834b4cc972b523d0481a
김승찬 <s24041@gsm.hs.kr>
2025-07-05 23:48:28 +0900

    add :: findAllByPlace() 메서드 추가

--- src/main/java/team/startup/gwangsan/domain/member/repository/MemberDetailRepository.java
+++ src/main/java/team/startup/gwangsan/domain/member/repository/MemberDetailRepository.java
@@ -3,6 +3,10 @@ package team.startup.gwangsan.domain.member.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import team.startup.gwangsan.domain.member.entity.MemberDetail;
 import team.startup.gwangsan.domain.member.repository.custom.MemberDetailCustomRepository;
+import team.startup.gwangsan.domain.place.entity.Place;
+
+import java.util.List;
 
 public interface MemberDetailRepository extends JpaRepository<MemberDetail, Long>, MemberDetailCustomRepository {
+    List<MemberDetail> findAllByPlace(Place place);
 }
