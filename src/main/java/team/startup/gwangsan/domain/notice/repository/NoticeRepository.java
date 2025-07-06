bcf01ac8da41148837ad99b98d945f7befb6eedb
김승찬 <s24041@gsm.hs.kr>
2025-07-06 22:01:43 +0900

    update :: 조회 하는 사람의 권한, 지점을 바탕으로 조회하도록 변경

--- src/main/java/team/startup/gwangsan/domain/notice/repository/NoticeRepository.java
+++ src/main/java/team/startup/gwangsan/domain/notice/repository/NoticeRepository.java
@@ -1,7 +1,11 @@
 package team.startup.gwangsan.domain.notice.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import team.startup.gwangsan.domain.notice.entity.Notice;
+import team.startup.gwangsan.domain.place.entity.Place;
 
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
+    Page<Notice> findAllByPlace(Place place, Pageable pageable);
 }
