8e65b37caf3acfa06e6ee0e422e8da31f65758ff
정재원 <s24049@gsm.hs.kr>
2025-07-07 17:06:21 +0900

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -2,13 +2,7 @@
 echo "== After Install 시작 =="
 
 # 실행 권한 부여
-chmod +x /var/www/myapp/gradlew
-chmod +x /var/www/myapp/start.sh
-
-# 디렉터리 이동
-cd /var/www/myapp
-
-# Gradle Wrapper로 빌드 실행
-./gradlew clean build -x test
+chmod +x /var/www/myapp/gradlew || true
+chmod +x /var/www/myapp/start.sh || true
 
 echo "== After Install 완료 =="
