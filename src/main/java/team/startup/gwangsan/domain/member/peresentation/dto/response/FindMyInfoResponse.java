b4471855f3b37a99e1685064d59355b46b1dd3a6
김승찬 <s24041@gsm.hs.kr>
2025-07-07 11:27:44 +0900

    add :: dto에 placeName추가

--- src/main/java/team/startup/gwangsan/domain/member/peresentation/dto/response/FindMyInfoResponse.java
+++ src/main/java/team/startup/gwangsan/domain/member/peresentation/dto/response/FindMyInfoResponse.java
@@ -3,6 +3,7 @@ package team.startup.gwangsan.domain.member.peresentation.dto.response;
 public record FindMyInfoResponse(
         Long memberId,
         String nickname,
+        String placeName,
         String profileUrl,
         Integer light,
         Integer gwangsan
