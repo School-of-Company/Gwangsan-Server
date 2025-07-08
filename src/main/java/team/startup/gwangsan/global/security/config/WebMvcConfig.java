4fa0c6d0b534ba3c5604cc5cbad078b93084d0b8
김승찬 <s24041@gsm.hs.kr>
2025-07-09 08:03:14 +0900

    update :: front url 환경 변수 처리

--- src/main/java/team/startup/gwangsan/global/security/config/WebMvcConfig.java
+++ src/main/java/team/startup/gwangsan/global/security/config/WebMvcConfig.java
@@ -4,13 +4,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import org.springframework.beans.factory.annotation.Value;
+
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Value("${cors.allowed-origins}")
+    private String[] allowedOrigins;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://gwangsan.vercel.app/")
+                .allowedOrigins(allowedOrigins)
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .exposedHeaders("Authorization")
