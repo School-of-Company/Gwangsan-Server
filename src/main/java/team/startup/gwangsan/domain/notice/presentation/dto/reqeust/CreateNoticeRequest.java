4ac1c1a2b86a9660a95128a51877c432dd44c30f
김승찬 <s24041@gsm.hs.kr>
2025-07-09 22:31:43 +0900

    update :: role 추가

--- src/main/java/team/startup/gwangsan/domain/notice/presentation/dto/reqeust/CreateNoticeRequest.java
+++ src/main/java/team/startup/gwangsan/domain/notice/presentation/dto/reqeust/CreateNoticeRequest.java
@@ -1,6 +1,7 @@
 package team.startup.gwangsan.domain.notice.presentation.dto.reqeust;
 
 import jakarta.validation.constraints.NotNull;
+import team.startup.gwangsan.domain.member.entity.constant.MemberRole;
 
 import java.util.List;
 
@@ -8,5 +9,6 @@ public record CreateNoticeRequest(
         @NotNull String title,
         @NotNull String content,
         @NotNull String placeName,
-        List<Long> imageIds
+        List<Long> imageIds,
+        List<MemberRole> roles
 ) {}
