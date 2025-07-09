5b3837f8284f9882d761cc23ac68769aed62efd2
김승찬 <s24041@gsm.hs.kr>
2025-07-09 17:04:35 +0900

    add :: RelatedKeywordController 추가

---
+++ src/main/java/team/startup/gwangsan/domain/relatedkeyword/presentation/RelatedKeywordController.java
@@ -0,0 +1,40 @@
+package team.startup.gwangsan.domain.relatedkeyword.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import team.startup.gwangsan.domain.relatedkeyword.presentation.dto.response.MyRelatedKeywordResponse;
+import team.startup.gwangsan.domain.relatedkeyword.presentation.dto.response.RelatedKeywordResponse;
+import team.startup.gwangsan.domain.relatedkeyword.service.DeleteMemberRelatedKeywordService;
+import team.startup.gwangsan.domain.relatedkeyword.service.FindMyRelatedKeywordService;
+import team.startup.gwangsan.domain.relatedkeyword.service.FindRelatedKeywordService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/related-keyword")
+@RequiredArgsConstructor
+public class RelatedKeywordController {
+
+    private final FindRelatedKeywordService findRelatedKeywordService;
+    private final FindMyRelatedKeywordService findMyRelatedKeywordService;
+    private final DeleteMemberRelatedKeywordService deleteMemberRelatedKeywordService;
+
+    @GetMapping
+    public ResponseEntity<List<RelatedKeywordResponse>> findAllKeywords() {
+        List<RelatedKeywordResponse> response = findRelatedKeywordService.execute();
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<List<MyRelatedKeywordResponse>> findMyKeywords() {
+        List<MyRelatedKeywordResponse> response = findMyRelatedKeywordService.execute();
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/{memberRelatedKeywordId}")
+    public ResponseEntity<Void> deleteMyKeyword(@PathVariable("memberRelatedKeywordId") Long id) {
+        deleteMemberRelatedKeywordService.execute(id);
+        return ResponseEntity.noContent().build();
+    }
+}
