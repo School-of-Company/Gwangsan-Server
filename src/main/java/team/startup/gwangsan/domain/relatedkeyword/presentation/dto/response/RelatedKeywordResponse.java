cc0a440be55565bc83e1ebb16dab23494829b34d
김승찬 <s24041@gsm.hs.kr>
2025-07-10 10:02:38 +0900

    update :: RelatedKeywordResponse로 통일

--- src/main/java/team/startup/gwangsan/domain/relatedkeyword/presentation/dto/response/RelatedKeywordResponse.java
+++ src/main/java/team/startup/gwangsan/domain/relatedkeyword/presentation/dto/response/RelatedKeywordResponse.java
@@ -1,6 +1,6 @@
 package team.startup.gwangsan.domain.relatedkeyword.presentation.dto.response;
 
 public record RelatedKeywordResponse(
-        Long keywordId,
+        Long relatedKeywordId,
         String name
 ) {}
