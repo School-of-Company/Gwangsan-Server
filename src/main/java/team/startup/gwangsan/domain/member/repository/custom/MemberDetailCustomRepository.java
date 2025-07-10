76bb4c6eb93296688f46ee2e3808434c0036d556
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-07-11 08:43:04 +0900

    update :: findPlaceNameMapByMemberIds 메서드 추가

--- src/main/java/team/startup/gwangsan/domain/member/repository/custom/MemberDetailCustomRepository.java
+++ src/main/java/team/startup/gwangsan/domain/member/repository/custom/MemberDetailCustomRepository.java
@@ -4,8 +4,13 @@ import team.startup.gwangsan.domain.member.entity.MemberDetail;
 import team.startup.gwangsan.domain.place.entity.Place;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public interface MemberDetailCustomRepository {
     Place findPlaceByMemberId(Long id);
+
     List<MemberDetail> findAllWithMember();
+
+    Map<Long, String> findPlaceNameMapByMemberIds(Set<Long> memberIds);
 }
