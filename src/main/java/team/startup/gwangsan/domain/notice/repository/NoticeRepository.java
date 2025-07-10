8b3dfd602742f9b2ab35b1a7e197b1706afd7ae1
김승찬 <s24041@gsm.hs.kr>
2025-07-10 11:37:52 +0900

    update :: 전체 공지 조회 데이터 수정 및 무한 스크롤 로직 추가

--- src/main/java/team/startup/gwangsan/domain/notice/repository/NoticeRepository.java
+++ src/main/java/team/startup/gwangsan/domain/notice/repository/NoticeRepository.java
@@ -1,6 +1,5 @@
 package team.startup.gwangsan.domain.notice.repository;
 
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import team.startup.gwangsan.domain.notice.entity.Notice;
@@ -9,7 +8,12 @@ import team.startup.gwangsan.domain.place.entity.Place;
 import java.util.List;
 
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
-    Page<Notice> findAllByPlace(Place place, Pageable pageable);
 
-    Page<Notice> findAllByPlaceIn(List<Place> places, Pageable pageable);
+    // 단일 장소
+    List<Notice> findByPlaceOrderByIdDesc(Place place, Pageable pageable);
+    List<Notice> findByPlaceAndIdLessThanOrderByIdDesc(Place place, Long lastId, Pageable pageable);
+
+    // HEAD_ADMIN용 (다중 장소)
+    List<Notice> findByPlaceInOrderByIdDesc(List<Place> places, Pageable pageable);
+    List<Notice> findByPlaceInAndIdLessThanOrderByIdDesc(List<Place> places, Long lastId, Pageable pageable);
 }
