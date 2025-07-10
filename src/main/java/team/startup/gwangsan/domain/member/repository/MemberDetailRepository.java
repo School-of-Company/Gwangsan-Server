c07970663df08a45b102d350c8fe72d50b127ba9
김승찬 <s24041@gsm.hs.kr>
2025-07-10 14:15:06 +0900

    update :: 사용자 권한을 바탕으로 지점의 멤버를 조회하도록 수정

--- src/main/java/team/startup/gwangsan/domain/member/repository/MemberDetailRepository.java
+++ src/main/java/team/startup/gwangsan/domain/member/repository/MemberDetailRepository.java
@@ -5,8 +5,15 @@ import team.startup.gwangsan.domain.member.entity.Member;
 import team.startup.gwangsan.domain.member.entity.MemberDetail;
 import team.startup.gwangsan.domain.member.repository.custom.MemberDetailCustomRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberDetailRepository extends JpaRepository<MemberDetail, Long>, MemberDetailCustomRepository {
+
     Optional<MemberDetail> findByMember(Member member);
+
+    List<MemberDetail> findAllByPlace_Head_Id(Integer headId);
+
+    List<MemberDetail> findAllByPlace_Id(Integer placeId);
 }
+
