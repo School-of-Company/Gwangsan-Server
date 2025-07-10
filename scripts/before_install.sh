c40391a93651a4bc24ed026b16fafb0ad3d89b8e
정재원 <s24049@gsm.hs.kr>
2025-07-10 17:01:32 +0900

    Delete scripts/before_install.sh

--- scripts/before_install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "== Before Install 시작 =="
-
-# 이전 배포 폴더 백업
-if [ -d /var/www/myapp ]; then
-  mv /var/www/myapp /var/www/myapp_backup_$(date +%Y%m%d%H%M%S)
-fi
-
-echo "== Before Install 완료 =="
