cae2b06770934958c37e4083d51089b9f1254637
김승찬 <s24041@gsm.hs.kr>
2025-07-11 22:26:23 +0900

    add :: findByReporterAndReportedAndContent 메서드 추가

--- src/main/java/team/startup/gwangsan/domain/report/repository/ReportRepository.java
+++ src/main/java/team/startup/gwangsan/domain/report/repository/ReportRepository.java
@@ -1,7 +1,12 @@
 package team.startup.gwangsan.domain.report.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import team.startup.gwangsan.domain.member.entity.Member;
 import team.startup.gwangsan.domain.report.entity.Report;
 
+import java.util.Optional;
+
 public interface ReportRepository extends JpaRepository<Report, Long> {
+    Optional<Report> findByReporterAndReportedAndContent(Member reporter, Member reported, String content);
+
 }
