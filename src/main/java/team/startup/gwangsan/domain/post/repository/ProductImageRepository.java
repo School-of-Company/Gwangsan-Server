a6e201124721e83fe0faf6f5ea36ac63e1f71e8c
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-07-11 13:42:22 +0900

    update :: ProductImageCustomRepository 상속 추가

--- src/main/java/team/startup/gwangsan/domain/post/repository/ProductImageRepository.java
+++ src/main/java/team/startup/gwangsan/domain/post/repository/ProductImageRepository.java
@@ -2,10 +2,11 @@ package team.startup.gwangsan.domain.post.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import team.startup.gwangsan.domain.post.entity.ProductImage;
+import team.startup.gwangsan.domain.post.repository.custom.ProductImageCustomRepository;
 
 import java.util.List;
 
-public interface ProductImageRepository extends JpaRepository<ProductImage, Long> {
+public interface ProductImageRepository extends JpaRepository<ProductImage, Long>, ProductImageCustomRepository {
     List<ProductImage> findByProductId(Long productId);
 
     void deleteByProductIdAndImageId(Long productId, Long imageId);
