68df2b5e165ca793bfc77eecf5d4111ea57eb590
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-07-12 22:19:49 +0900

    update :: 거래 완료 요청 조회 응답 dto 추가

--- src/main/java/team/startup/gwangsan/domain/admin/presentation/dto/response/GetAdminAlertResponse.java
+++ src/main/java/team/startup/gwangsan/domain/admin/presentation/dto/response/GetAdminAlertResponse.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public record GetAdminAlertResponse(
         List<GetReportAlertResponse> reports,
-        List<GetSignUpAlertResponse> signUps
+        List<GetSignUpAlertResponse> signUps,
+        List<GetTradeCompleteAlertResponse> trades
 ) {
 }
