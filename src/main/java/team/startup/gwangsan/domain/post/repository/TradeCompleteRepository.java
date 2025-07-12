a48a66c3c1a3396e1cf9c7966988d09537ac5da6
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-07-12 22:22:44 +0900

    add :: TradeCompleteRepository 추가

---
+++ src/main/java/team/startup/gwangsan/domain/post/repository/TradeCompleteRepository.java
@@ -0,0 +1,7 @@
+package team.startup.gwangsan.domain.post.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import team.startup.gwangsan.domain.post.entity.TradeComplete;
+
+public interface TradeCompleteRepository extends JpaRepository<TradeComplete, Long> {
+}
