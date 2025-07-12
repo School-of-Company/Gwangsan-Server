ec7eae5842c390c8eadbd1d761d07d3108101a62
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-07-12 21:33:53 +0900

    update :: 메서드 추가

--- src/main/java/team/startup/gwangsan/domain/admin/repository/AdminAlertRepository.java
+++ src/main/java/team/startup/gwangsan/domain/admin/repository/AdminAlertRepository.java
@@ -2,6 +2,12 @@ package team.startup.gwangsan.domain.admin.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import team.startup.gwangsan.domain.admin.entity.AdminAlert;
+import team.startup.gwangsan.domain.admin.entity.constant.AlertType;
+
+import java.util.Optional;
 
 public interface AdminAlertRepository extends JpaRepository<AdminAlert, Long> {
+    Optional<AdminAlert> findBySourceId(Long sourceId);
+
+    Optional<AdminAlert> findBySourceIdAndType(Long sourceId, AlertType type);
 }
