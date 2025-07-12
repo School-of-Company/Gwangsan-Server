ed6a4c7fffcee4c1f2b142c24784218b9b6023a5
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-07-12 22:18:34 +0900

    add :: CreateTradeCompleteAlertService 추가

---
+++ src/main/java/team/startup/gwangsan/domain/admin/service/CreateTradeCompleteAlertService.java
@@ -0,0 +1,8 @@
+package team.startup.gwangsan.domain.admin.service;
+
+import team.startup.gwangsan.domain.admin.entity.constant.AlertType;
+import team.startup.gwangsan.domain.member.entity.Member;
+
+public interface CreateTradeCompleteAlertService {
+    void execute(AlertType type, Long sourceId, Member otherMember, Member requesterMember);
+}
