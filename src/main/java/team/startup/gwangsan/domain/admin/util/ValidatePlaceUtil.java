d566fdb7d8ed97efb7f1b04b3557ceb80fa537c9
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-07-12 22:23:25 +0900

    update :: 지점 비교 로직 수정

--- src/main/java/team/startup/gwangsan/domain/admin/util/ValidatePlaceUtil.java
+++ src/main/java/team/startup/gwangsan/domain/admin/util/ValidatePlaceUtil.java
@@ -3,26 +3,34 @@ package team.startup.gwangsan.domain.admin.util;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import team.startup.gwangsan.domain.member.entity.Member;
+import team.startup.gwangsan.domain.member.entity.MemberDetail;
 import team.startup.gwangsan.domain.member.entity.constant.MemberRole;
-import team.startup.gwangsan.domain.member.repository.MemberDetailRepository;
-import team.startup.gwangsan.domain.member.repository.custom.MemberDetailCustomRepository;
 import team.startup.gwangsan.domain.place.entity.Place;
 import team.startup.gwangsan.domain.place.exception.PlaceMismatchException;
+import team.startup.gwangsan.domain.place.repository.PlaceRepository;
+
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class ValidatePlaceUtil {
 
-    private final MemberDetailRepository memberDetailRepository;
+    private final PlaceRepository placeRepository;
 
-    public void validateSamePlace(Member admin, Member target) {
-        if (!admin.getRole().equals(MemberRole.ROLE_HEAD_ADMIN)) {
-            Place adminPlace = memberDetailRepository.findPlaceByMemberId(admin.getId());
-            Place targetPlace = memberDetailRepository.findPlaceByMemberId(target.getId());
+    public void validateSamePlace(Member admin, MemberDetail adminDetail, MemberDetail targetDetail) {
+        List<Place> adminPlaces = getAdminPlaces(admin, adminDetail);
+        validatePlaceAuthority(adminPlaces, targetDetail.getPlace());
+    }
 
-            if (!targetPlace.isSamePlace(adminPlace)) {
-                throw new PlaceMismatchException();
-            }
-        }
+    private List<Place> getAdminPlaces(Member admin, MemberDetail detail) {
+        return admin.getRole() == MemberRole.ROLE_HEAD_ADMIN
+                ? placeRepository.findByHead(detail.getPlace().getHead())
+                : List.of(detail.getPlace());
+    }
+
+    private void validatePlaceAuthority(List<Place> places, Place targetPlace) {
+        boolean hasAuthority = places.stream()
+                .anyMatch(p -> p.getId().equals(targetPlace.getId()));
+        if (!hasAuthority) throw new PlaceMismatchException();
     }
 }
