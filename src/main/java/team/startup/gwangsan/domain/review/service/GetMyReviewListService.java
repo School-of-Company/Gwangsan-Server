e0cbc9a9625a3409556b5fb77a8e7fe4061b3cac
김승찬 <s24041@gsm.hs.kr>
2025-07-14 08:30:24 +0900

    update :: type mode 제거

--- src/main/java/team/startup/gwangsan/domain/review/service/GetMyReviewListService.java
+++ src/main/java/team/startup/gwangsan/domain/review/service/GetMyReviewListService.java
@@ -1,11 +1,9 @@
 package team.startup.gwangsan.domain.review.service;
 
-import team.startup.gwangsan.domain.post.entity.constant.Mode;
-import team.startup.gwangsan.domain.post.entity.constant.Type;
 import team.startup.gwangsan.domain.review.presentation.dto.response.ReviewResponse;
 
 import java.util.List;
 
 public interface GetMyReviewListService {
-    List<ReviewResponse> execute(Type type, Mode mode);
+    List<ReviewResponse> execute();
 }
