9c3ff8ed35ac6eedc91e9ed75a7c0621a9a98fe4
김승찬 <s24041@gsm.hs.kr>
2025-07-14 08:31:27 +0900

    update :: 해당 프로덕트의 이미지를 받아오도록 수정

--- src/main/java/team/startup/gwangsan/domain/review/service/impl/GetReviewByMemberServiceImpl.java
+++ src/main/java/team/startup/gwangsan/domain/review/service/impl/GetReviewByMemberServiceImpl.java
@@ -3,9 +3,11 @@ package team.startup.gwangsan.domain.review.service.impl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import team.startup.gwangsan.domain.image.presentation.dto.response.GetImageResponse;
 import team.startup.gwangsan.domain.member.entity.Member;
 import team.startup.gwangsan.domain.member.exception.NotFoundMemberException;
 import team.startup.gwangsan.domain.member.repository.MemberRepository;
+import team.startup.gwangsan.domain.post.repository.ProductImageRepository;
 import team.startup.gwangsan.domain.review.presentation.dto.response.ReviewResponse;
 import team.startup.gwangsan.domain.review.repository.ReviewRepository;
 import team.startup.gwangsan.domain.review.service.GetReviewByMemberService;
@@ -18,6 +20,7 @@ public class GetReviewByMemberServiceImpl implements GetReviewByMemberService {
 
     private final MemberRepository memberRepository;
     private final ReviewRepository reviewRepository;
+    private final ProductImageRepository productImageRepository;
 
     @Override
     @Transactional(readOnly = true)
@@ -26,12 +29,22 @@ public class GetReviewByMemberServiceImpl implements GetReviewByMemberService {
                 .orElseThrow(NotFoundMemberException::new);
 
         return reviewRepository.findAllByReviewed(reviewedMember).stream()
-                .map(review -> new ReviewResponse(
-                        review.getProduct().getId(),
-                        review.getContent(),
-                        review.getLight(),
-                        review.getReviewer().getNickname()
-                ))
+                .map(review -> {
+                    var images = productImageRepository.findAllByProduct(review.getProduct()).stream()
+                            .map(pi -> new GetImageResponse(
+                                    pi.getImage().getId(),
+                                    pi.getImage().getImageUrl()
+                            ))
+                            .toList();
+
+                    return new ReviewResponse(
+                            review.getProduct().getId(),
+                            review.getContent(),
+                            review.getLight(),
+                            review.getReviewer().getNickname(),
+                            images
+                    );
+                })
                 .toList();
     }
 }
