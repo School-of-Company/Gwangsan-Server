9e3584651514d0fff00f2bc936e021a449fba93b
김승찬 <s24041@gsm.hs.kr>
2025-07-14 08:31:14 +0900

    update :: 해당 프로덕트의 이미지를 받아오도록 수정

--- src/main/java/team/startup/gwangsan/domain/review/presentation/dto/response/ReviewResponse.java
+++ src/main/java/team/startup/gwangsan/domain/review/presentation/dto/response/ReviewResponse.java
@@ -1,8 +1,13 @@
 package team.startup.gwangsan.domain.review.presentation.dto.response;
 
+import team.startup.gwangsan.domain.image.presentation.dto.response.GetImageResponse;
+
+import java.util.List;
+
 public record ReviewResponse(
         Long productId,
         String content,
         Integer light,
-        String reviewerName
+        String reviewerName,
+        List<GetImageResponse> imageUrls
 ) {}
