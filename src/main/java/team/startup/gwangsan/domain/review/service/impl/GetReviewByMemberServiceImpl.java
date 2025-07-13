48796179e870f87907b00b082cc609f268817bd5
김승찬 <s24041@gsm.hs.kr>
2025-07-13 12:45:24 +0900

    add :: GetReviewByMemberServiceImpl 추가

---
+++ src/main/java/team/startup/gwangsan/domain/review/service/impl/GetReviewByMemberServiceImpl.java
@@ -0,0 +1,38 @@
+package team.startup.gwangsan.domain.review.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import team.startup.gwangsan.domain.member.entity.Member;
+import team.startup.gwangsan.domain.member.exception.NotFoundMemberException;
+import team.startup.gwangsan.domain.member.repository.MemberRepository;
+import team.startup.gwangsan.domain.review.presentation.dto.response.ReviewResponse;
+import team.startup.gwangsan.domain.review.repository.ReviewRepository;
+import team.startup.gwangsan.domain.review.service.GetReviewByMemberService;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class GetReviewByMemberServiceImpl implements GetReviewByMemberService {
+
+    private final MemberRepository memberRepository;
+    private final ReviewRepository reviewRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<ReviewResponse> execute(Long memberId) {
+        Member reviewedMember = memberRepository.findById(memberId)
+                .orElseThrow(NotFoundMemberException::new);
+
+        return reviewRepository.findAllByReviewed(reviewedMember).stream()
+                .map(review -> new ReviewResponse(
+                        review.getProduct().getId(),
+                        review.getContent(),
+                        review.getLight(),
+                        review.getReviewer().getNickname()
+                ))
+                .toList();
+    }
+}
+
