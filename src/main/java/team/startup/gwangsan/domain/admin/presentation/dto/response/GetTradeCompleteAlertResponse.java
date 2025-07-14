8f24e5d1f087b2e6423bd00d6bb041053d4dec4d
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-07-14 23:18:14 +0900

    update :: placeName 필드 수정

--- src/main/java/team/startup/gwangsan/domain/admin/presentation/dto/response/GetTradeCompleteAlertResponse.java
+++ src/main/java/team/startup/gwangsan/domain/admin/presentation/dto/response/GetTradeCompleteAlertResponse.java
@@ -7,7 +7,6 @@ import java.time.LocalDateTime;
 public record GetTradeCompleteAlertResponse(
         String nickname,
         String title,
-        String placeName,
         LocalDateTime createdAt,
         GetProductResponse product
 ) {
