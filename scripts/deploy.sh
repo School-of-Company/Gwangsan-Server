de6ae9eb1923c559ddcfa0a4a08968999423a38b
정재원 <s24049@gsm.hs.kr>
2025-07-15 12:03:04 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-IMAGE_NAME=gwangsan-app
+ECR_REPO=gwangsan-ecr
+REGION=ap-northeast-2
+IMAGE_TAG=latest
 CONTAINER_NAME=gwangsan-container
-JAVA_HOME=/usr/lib/jvm/jdk-21.0.7
+ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
 
-docker load < /home/ec2-user/$IMAGE_NAME.tar
+# ECR 로그인
+aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com
 
+# 기존 컨테이너 중지 및 제거
+docker stop $CONTAINER_NAME || true
+docker rm $CONTAINER_NAME || true
 
-docker rm -f $CONTAINER_NAME || true
-
-docker run -d --name $CONTAINER_NAME -p 8080:8080 \
-  --env-file /home/ec2-user/.env \
-  $IMAGE_NAME
+# 최신 이미지 pull 및 실행
+docker pull $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$ECR_REPO:$IMAGE_TAG
+docker run -d --name $CONTAINER_NAME -p 80:8080 $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$ECR_REPO:$IMAGE_TAG
