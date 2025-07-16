d2e91b04352dfdd9f30723b3911424a9abbd50ef
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-07-16 22:35:37 +0900

    update :: 오버로드 메서드 삭제

--- src/main/java/team/startup/gwangsan/domain/post/service/FindProductByIdService.java
+++ src/main/java/team/startup/gwangsan/domain/post/service/FindProductByIdService.java
@@ -1,10 +1,7 @@
 package team.startup.gwangsan.domain.post.service;
 
-import team.startup.gwangsan.domain.member.entity.Member;
-import team.startup.gwangsan.domain.post.presentation.dto.response.GetProductResponse;
+import team.startup.gwangsan.domain.post.presentation.dto.response.GetProductByIdResponse;
 
 public interface FindProductByIdService {
-    GetProductResponse execute(Long id);
-
-    GetProductResponse execute(Member member, Long id);
+    GetProductByIdResponse execute(Long id);
 }
