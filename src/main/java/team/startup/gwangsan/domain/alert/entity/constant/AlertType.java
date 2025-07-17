8314e2d1dca369871d35ea0dd3e6a44278f3ae6b
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-07-17 09:33:51 +0900

    add :: AlertType enum 클래스 추가

---
+++ src/main/java/team/startup/gwangsan/domain/alert/entity/constant/AlertType.java
@@ -0,0 +1,11 @@
+package team.startup.gwangsan.domain.alert.entity.constant;
+
+public enum AlertType {
+    CHATTING_REQUEST,
+    NOTICE,
+    TRADE_COMPLETE,
+    TRADE_COMPLETE_REJECT,
+    OTHER_MEMBER_TRADE_COMPLETE,
+    RECOMMENDER,
+    REVIEW,
+}
