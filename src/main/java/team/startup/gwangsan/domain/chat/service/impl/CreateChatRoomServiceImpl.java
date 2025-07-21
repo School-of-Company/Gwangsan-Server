ee95ad1bbb510ad2abbefc9c8948e1e2e1c20c92
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-07-21 16:25:56 +0900

    add :: CreateChatRoomService 구현

---
+++ src/main/java/team/startup/gwangsan/domain/chat/service/impl/CreateChatRoomServiceImpl.java
@@ -0,0 +1,58 @@
+package team.startup.gwangsan.domain.chat.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import team.startup.gwangsan.domain.chat.entity.ChatRoom;
+import team.startup.gwangsan.domain.chat.presentation.dto.response.CreateChatRoomResponse;
+import team.startup.gwangsan.domain.chat.repository.ChatRoomRepository;
+import team.startup.gwangsan.domain.chat.service.CreateChatRoomService;
+import team.startup.gwangsan.domain.member.entity.Member;
+import team.startup.gwangsan.domain.post.entity.Product;
+import team.startup.gwangsan.domain.post.exception.NotFoundProductException;
+import team.startup.gwangsan.domain.post.repository.ProductRepository;
+import team.startup.gwangsan.global.util.MemberUtil;
+
+@Service
+@RequiredArgsConstructor
+public class CreateChatRoomServiceImpl implements CreateChatRoomService {
+
+    private final ChatRoomRepository chatRoomRepository;
+    private final MemberUtil memberUtil;
+    private final ProductRepository productRepository;
+
+    @Override
+    @Transactional
+    public CreateChatRoomResponse execute(Long productId) {
+        Member member = memberUtil.getCurrentMember();
+
+        Product product = productRepository.findById(productId)
+                .orElseThrow(NotFoundProductException::new);
+        Member productMember = product.getMember();
+
+        Member member1;
+        Member member2;
+        if (member.getId() < productMember.getId()) {
+            member1 = member;
+            member2 = productMember;
+        } else {
+            member1 = productMember;
+            member2 = member;
+        }
+
+        ChatRoom existsChatRoom = chatRoomRepository.findByProductIdAndMember1AndMember2(productId, member1, member2)
+                .orElse(null);
+        if (existsChatRoom != null) {
+            return new CreateChatRoomResponse(existsChatRoom.getId());
+        }
+
+        ChatRoom chatRoom = ChatRoom.builder()
+                .member1(member1)
+                .member2(member2)
+                .isActive(true)
+                .product(product)
+                .build();
+
+        return new CreateChatRoomResponse(chatRoomRepository.save(chatRoom).getId());
+    }
+}
