fd5bff40ec3534b8b4db5ead9d2c4bb477345c01
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-07-21 16:26:56 +0900

    add :: FindRoomIdByProductIdService 구현

---
+++ src/main/java/team/startup/gwangsan/domain/chat/service/impl/FindRoomIdByProductIdServiceImpl.java
@@ -0,0 +1,30 @@
+package team.startup.gwangsan.domain.chat.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import team.startup.gwangsan.domain.chat.entity.ChatRoom;
+import team.startup.gwangsan.domain.chat.exception.NotFoundChatRoomException;
+import team.startup.gwangsan.domain.chat.presentation.dto.response.GetRoomIdResponse;
+import team.startup.gwangsan.domain.chat.repository.ChatRoomRepository;
+import team.startup.gwangsan.domain.chat.service.FindRoomIdByProductIdService;
+import team.startup.gwangsan.domain.member.entity.Member;
+import team.startup.gwangsan.global.util.MemberUtil;
+
+@Service
+@RequiredArgsConstructor
+public class FindRoomIdByProductIdServiceImpl implements FindRoomIdByProductIdService {
+
+    private final ChatRoomRepository chatRoomRepository;
+    private final MemberUtil memberUtil;
+
+    @Override
+    @Transactional
+    public GetRoomIdResponse execute(Long productId) {
+        Member member = memberUtil.getCurrentMember();
+        ChatRoom chatRoom = chatRoomRepository.findByProductIdAndMember(productId, member)
+                .orElseThrow(NotFoundChatRoomException::new);
+
+        return new GetRoomIdResponse(chatRoom.getId());
+    }
+}
