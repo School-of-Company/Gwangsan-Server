992a0032847a739ab210d114722679cffd82ce79
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-07-21 16:28:49 +0900

    add :: FindRoomsByCurrentUserService 구현

---
+++ src/main/java/team/startup/gwangsan/domain/chat/service/impl/FindRoomsByCurrentUserServiceImpl.java
@@ -0,0 +1,23 @@
+package team.startup.gwangsan.domain.chat.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import team.startup.gwangsan.domain.chat.presentation.dto.response.GetRoomsResponse;
+import team.startup.gwangsan.domain.chat.repository.ChatRoomRepository;
+import team.startup.gwangsan.domain.chat.service.FindRoomsByCurrentUserService;
+import team.startup.gwangsan.global.util.MemberUtil;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class FindRoomsByCurrentUserServiceImpl implements FindRoomsByCurrentUserService {
+
+    private final ChatRoomRepository chatRoomRepository;
+    private final MemberUtil memberUtil;
+
+    @Override
+    public List<GetRoomsResponse> execute() {
+        return chatRoomRepository.findRoomsByMemberId(memberUtil.getCurrentMember().getId());
+    }
+}
