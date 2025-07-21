16c76cbf154e1d99d2f82c24ecd912109c31e710
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-07-21 20:45:02 +0900

    update :: readOnly = true 추가

--- src/main/java/team/startup/gwangsan/domain/chat/service/impl/FindRoomsByCurrentUserServiceImpl.java
+++ src/main/java/team/startup/gwangsan/domain/chat/service/impl/FindRoomsByCurrentUserServiceImpl.java
@@ -2,6 +2,7 @@ package team.startup.gwangsan.domain.chat.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import team.startup.gwangsan.domain.chat.presentation.dto.response.GetRoomsResponse;
 import team.startup.gwangsan.domain.chat.repository.ChatRoomRepository;
 import team.startup.gwangsan.domain.chat.service.FindRoomsByCurrentUserService;
@@ -17,6 +18,7 @@ public class FindRoomsByCurrentUserServiceImpl implements FindRoomsByCurrentUser
     private final MemberUtil memberUtil;
 
     @Override
+    @Transactional(readOnly = true)
     public List<GetRoomsResponse> execute() {
         return chatRoomRepository.findRoomsByMemberId(memberUtil.getCurrentMember().getId());
     }
