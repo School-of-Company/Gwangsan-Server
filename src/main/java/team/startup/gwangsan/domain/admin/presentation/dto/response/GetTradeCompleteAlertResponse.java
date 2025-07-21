f745ae8e70312da35b0a3d01f80bef4788795072
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-07-22 08:48:57 +0900

    update :: id 필드 추가

--- src/main/java/team/startup/gwangsan/domain/admin/presentation/dto/response/GetTradeCompleteAlertResponse.java
+++ src/main/java/team/startup/gwangsan/domain/admin/presentation/dto/response/GetTradeCompleteAlertResponse.java
@@ -5,6 +5,7 @@ import team.startup.gwangsan.domain.post.presentation.dto.response.GetProductRes
 import java.time.LocalDateTime;
 
 public record GetTradeCompleteAlertResponse(
+        Long id,
         String nickname,
         String title,
         LocalDateTime createdAt,
