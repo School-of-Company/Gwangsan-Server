d61669b52fe8919a0cf89cf6ac50be9842ab9a21
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-07-22 16:37:14 +0900

    update :: Member 객체 id로 변경

--- src/main/java/team/startup/gwangsan/global/event/CreateAdminAlertEvent.java
+++ src/main/java/team/startup/gwangsan/global/event/CreateAdminAlertEvent.java
@@ -1,15 +1,14 @@
 package team.startup.gwangsan.global.event;
 
 import team.startup.gwangsan.domain.admin.entity.constant.AlertType;
-import team.startup.gwangsan.domain.member.entity.Member;
 
 public record CreateAdminAlertEvent(
         AlertType type,
         Long sourceId,
-        Member member,
-        Member otherMember
+        Long memberId,
+        Long otherMemberId
 ) {
-    public CreateAdminAlertEvent(AlertType type, Long sourceId, Member member) {
-        this(type, sourceId, member, null);
+    public CreateAdminAlertEvent(AlertType type, Long sourceId, Long memberId) {
+        this(type, sourceId, memberId, null);
     }
 }
