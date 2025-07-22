bcab8e8ab45a437d99798b82436887264f0f9c39
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-07-22 16:41:17 +0900

    update :: 파라미터 id 값으로 변경

--- src/main/java/team/startup/gwangsan/domain/admin/service/impl/CreateTradeCompleteAlertServiceImpl.java
+++ src/main/java/team/startup/gwangsan/domain/admin/service/impl/CreateTradeCompleteAlertServiceImpl.java
@@ -1,6 +1,7 @@
 package team.startup.gwangsan.domain.admin.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import team.startup.gwangsan.domain.admin.entity.AdminAlert;
@@ -8,9 +9,16 @@ import team.startup.gwangsan.domain.admin.entity.constant.AlertType;
 import team.startup.gwangsan.domain.admin.repository.AdminAlertRepository;
 import team.startup.gwangsan.domain.admin.service.CreateTradeCompleteAlertService;
 import team.startup.gwangsan.domain.member.entity.Member;
+import team.startup.gwangsan.domain.member.repository.MemberRepository;
 import team.startup.gwangsan.domain.post.entity.Product;
 import team.startup.gwangsan.domain.post.exception.NotFoundProductException;
 import team.startup.gwangsan.domain.post.repository.ProductRepository;
+import team.startup.gwangsan.global.event.CreateAlertEvent;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -18,14 +26,23 @@ public class CreateTradeCompleteAlertServiceImpl implements CreateTradeCompleteA
 
     private final AdminAlertRepository adminAlertRepository;
     private final ProductRepository productRepository;
+    private final MemberRepository memberRepository;
+    private final ApplicationEventPublisher applicationEventPublisher;
 
     private static final String TRADE_COMPLETE_TITLE = "거래 완료 요청";
 
     @Override
     @Transactional
-    public void execute(AlertType type, Long sourceId, Member otherMember, Member requesterMember) {
+    public void execute(AlertType type, Long sourceId, Long otherMemberId, Long requesterMemberId) {
         Product product = productRepository.findById(sourceId)
                 .orElseThrow(NotFoundProductException::new);
+
+        Map<Long, Member> memberMap = memberRepository.findAllByIdIn(List.of(otherMemberId, requesterMemberId))
+                .stream()
+                .collect(Collectors.toMap(Member::getId, Function.identity()));
+
+        Member otherMember = memberMap.get(otherMemberId);
+        Member requesterMember = memberMap.get(requesterMemberId);
 
         AdminAlert alert = AdminAlert.builder()
                 .type(type)
