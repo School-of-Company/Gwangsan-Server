ecd38a47ddae5a32811d76153d1bb8d648495624
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-07-23 00:29:41 +0900

    update :: findAllByIdIn 메서드 추가

--- src/main/java/team/startup/gwangsan/domain/report/repository/ReportRepository.java
+++ src/main/java/team/startup/gwangsan/domain/report/repository/ReportRepository.java
@@ -5,8 +5,11 @@ import team.startup.gwangsan.domain.member.entity.Member;
 import team.startup.gwangsan.domain.report.entity.Report;
 import team.startup.gwangsan.domain.report.entity.constant.ReportType;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ReportRepository extends JpaRepository<Report, Long> {
     Optional<Report> findByReporterAndReportedAndReportType(Member reporter, Member reported, ReportType reportType);
+
+    List<Report> findAllByIdIn(List<Long> ids);
 }
