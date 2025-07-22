629562683ca74321b50cb082c52eb3e5e4550c30
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-07-23 03:33:48 +0900

    add :: SuspendController 추가

---
+++ src/main/java/team/startup/gwangsan/domain/suspend/presentation/SuspendController.java
@@ -0,0 +1,25 @@
+package team.startup.gwangsan.domain.suspend.presentation;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import team.startup.gwangsan.domain.suspend.presentation.dto.request.SuspendMemberRequest;
+import team.startup.gwangsan.domain.suspend.service.SuspendMemberService;
+
+@RestController
+@RequestMapping("/api/suspend")
+@RequiredArgsConstructor
+public class SuspendController {
+
+    private final SuspendMemberService suspendMemberService;
+
+    @PatchMapping
+    public ResponseEntity<Void> suspend(@RequestBody @Valid SuspendMemberRequest request) {
+        suspendMemberService.execute(request.memberId(), request.suspendedDays());
+        return ResponseEntity.noContent().build();
+    }
+}
