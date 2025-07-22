0082eade7085cb60679b2ff5009c9179bb55aa9b
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-07-23 03:33:55 +0900

    add :: SuspendMemberRequest 추가

---
+++ src/main/java/team/startup/gwangsan/domain/suspend/presentation/dto/request/SuspendMemberRequest.java
@@ -0,0 +1,9 @@
+package team.startup.gwangsan.domain.suspend.presentation.dto.request;
+
+import jakarta.validation.constraints.NotNull;
+
+public record SuspendMemberRequest(
+        @NotNull Long memberId,
+        @NotNull int suspendedDays
+) {
+}
