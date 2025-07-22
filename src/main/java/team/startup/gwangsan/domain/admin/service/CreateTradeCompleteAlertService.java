83e21b7ff795f4b83d15e5f15d9fa9886757d3ae
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-07-22 16:41:14 +0900

    update :: 파라미터 id 값으로 변경

--- src/main/java/team/startup/gwangsan/domain/admin/service/CreateTradeCompleteAlertService.java
+++ src/main/java/team/startup/gwangsan/domain/admin/service/CreateTradeCompleteAlertService.java
@@ -1,8 +1,7 @@
 package team.startup.gwangsan.domain.admin.service;
 
 import team.startup.gwangsan.domain.admin.entity.constant.AlertType;
-import team.startup.gwangsan.domain.member.entity.Member;
 
 public interface CreateTradeCompleteAlertService {
-    void execute(AlertType type, Long sourceId, Member otherMember, Member requesterMember);
+    void execute(AlertType type, Long sourceId, Long otherMemberId, Long requesterMemberId);
 }
