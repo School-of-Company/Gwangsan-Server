cc19aeeb849bdebf7f295d1257a0d059d160d026
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-07-22 16:45:04 +0900

    update :: 비동기 스레드 명시

--- src/main/java/team/startup/gwangsan/global/event/handler/SendNotificationEventListener.java
+++ src/main/java/team/startup/gwangsan/global/event/handler/SendNotificationEventListener.java
@@ -14,7 +14,7 @@ public class SendNotificationEventListener {
 
     private final SendNotificationService sendNotificationService;
 
-    @Async
+    @Async("asyncExecutor")
     @TransactionalEventListener(value = SendNotificationEvent.class, phase = TransactionPhase.AFTER_COMMIT)
     public void handleNotification(SendNotificationEvent event) {
         sendNotificationService.execute(event.deviceTokens(), event.type());
