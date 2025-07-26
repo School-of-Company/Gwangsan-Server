b4738c42e91b65f6d3836af7212fb6b6b4412858
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-07-26 17:22:44 +0900

    update :: 서비스 매핑 추가

--- src/main/java/team/startup/gwangsan/domain/alert/presentation/AlertController.java
+++ src/main/java/team/startup/gwangsan/domain/alert/presentation/AlertController.java
@@ -2,11 +2,12 @@ package team.startup.gwangsan.domain.alert.presentation;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import team.startup.gwangsan.domain.alert.presentation.dto.response.GetAlertResponse;
+import team.startup.gwangsan.domain.alert.presentation.dto.response.ExistsAlertResponse;
+import team.startup.gwangsan.domain.alert.service.ExistsUnreadAlertService;
 import team.startup.gwangsan.domain.alert.service.FindAlertByCurrentService;
+import team.startup.gwangsan.domain.alert.service.ReadAlertService;
 
 import java.util.List;
 
@@ -16,10 +17,24 @@ import java.util.List;
 public class AlertController {
 
     private final FindAlertByCurrentService findAlertByCurrentService;
+    private final ExistsUnreadAlertService existsUnreadAlertService;
+    private final ReadAlertService readAlertService;
 
     @GetMapping
     public ResponseEntity<List<GetAlertResponse>> getAlerts() {
         List<GetAlertResponse> responses = findAlertByCurrentService.execute();
         return ResponseEntity.ok(responses);
+    }
+
+    @GetMapping("/unread")
+    public ResponseEntity<ExistsAlertResponse> getUnreadAlerts() {
+        ExistsAlertResponse responses = existsUnreadAlertService.execute();
+        return ResponseEntity.ok(responses);
+    }
+
+    @PatchMapping("/read/{alert_id}")
+    public ResponseEntity<Void> readAlerts(@PathVariable("alert_id") Long alertId) {
+        readAlertService.execute(alertId);
+        return ResponseEntity.noContent().build();
     }
 }
