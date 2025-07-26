e796d30fcb46f297969fb4c1fe0a30043d5e5630
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-07-26 17:18:41 +0900

    add :: ExistsUnreadAlertService 추가

---
+++ src/main/java/team/startup/gwangsan/domain/alert/service/ExistsUnreadAlertService.java
@@ -0,0 +1,7 @@
+package team.startup.gwangsan.domain.alert.service;
+
+import team.startup.gwangsan.domain.alert.presentation.dto.response.ExistsAlertResponse;
+
+public interface ExistsUnreadAlertService {
+    ExistsAlertResponse execute();
+}
