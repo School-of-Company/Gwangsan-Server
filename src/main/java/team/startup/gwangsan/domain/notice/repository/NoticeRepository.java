b87a27cd513be5e48af9672c85ddc5b1b66a0585
seungchan <s24041@gsm.hs.kr>
2025-07-27 18:38:28 +0900

    update :: role 추가에 따른 메서드 수정

--- src/main/java/team/startup/gwangsan/domain/notice/repository/NoticeRepository.java
+++ src/main/java/team/startup/gwangsan/domain/notice/repository/NoticeRepository.java
@@ -2,18 +2,19 @@ package team.startup.gwangsan.domain.notice.repository;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import team.startup.gwangsan.domain.member.entity.constant.MemberRole;
 import team.startup.gwangsan.domain.notice.entity.Notice;
 import team.startup.gwangsan.domain.place.entity.Place;
 
 import java.util.List;
 
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
+    List<Notice> findByPlaceInAndTargetRolesContainingOrderByIdDesc(List<Place> places, MemberRole role, Pageable pageable);
 
-    // 단일 장소
-    List<Notice> findByPlaceOrderByIdDesc(Place place, Pageable pageable);
-    List<Notice> findByPlaceAndIdLessThanOrderByIdDesc(Place place, Long lastId, Pageable pageable);
+    List<Notice> findByPlaceInAndTargetRolesContainingAndIdLessThanOrderByIdDesc(List<Place> places, MemberRole role, Long lastId, Pageable pageable);
 
-    // HEAD_ADMIN용 (다중 장소)
-    List<Notice> findByPlaceInOrderByIdDesc(List<Place> places, Pageable pageable);
-    List<Notice> findByPlaceInAndIdLessThanOrderByIdDesc(List<Place> places, Long lastId, Pageable pageable);
+    List<Notice> findByPlaceAndTargetRolesContainingOrderByIdDesc(Place place, MemberRole role, Pageable pageable);
+
+    List<Notice> findByPlaceAndTargetRolesContainingAndIdLessThanOrderByIdDesc(Place place, MemberRole role, Long lastId, Pageable pageable);
+
 }
