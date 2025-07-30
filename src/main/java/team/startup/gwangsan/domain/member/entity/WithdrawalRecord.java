ace486ef9a4f6b3b6ec467f7a36d8a4ec2821749
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-07-30 23:18:18 +0900

    add :: 회원 탈퇴 엔티티 추가

---
+++ src/main/java/team/startup/gwangsan/domain/member/entity/WithdrawalRecord.java
@@ -0,0 +1,39 @@
+package team.startup.gwangsan.domain.member.entity;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "tbl_withdrawal_record")
+@EntityListeners(AuditingEntityListener.class)
+public class WithdrawalRecord {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "withdrawal_record_id")
+    private Long id;
+
+    @Column(name = "phone_number", nullable = false)
+    private String phoneNumber;
+
+    @Column(name = "gwangsan", nullable = false)
+    private Integer gwangsan;
+
+    @CreatedDate
+    @Column(name = "withdrawal_at", nullable = false)
+    private LocalDateTime withdrawalAt;
+
+    @Builder
+    public WithdrawalRecord(String phoneNumber, Integer gwangsan) {
+        this.phoneNumber = phoneNumber;
+        this.gwangsan = gwangsan;
+    }
+}
