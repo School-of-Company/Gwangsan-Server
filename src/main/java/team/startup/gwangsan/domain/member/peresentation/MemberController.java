d388935e8a2940fb5dd49817197c34e6be2a2ef2
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-07-30 23:19:01 +0900

    update :: MemberWithdrawalService 매핑 추가

--- src/main/java/team/startup/gwangsan/domain/member/peresentation/MemberController.java
+++ src/main/java/team/startup/gwangsan/domain/member/peresentation/MemberController.java
@@ -2,16 +2,15 @@ package team.startup.gwangsan.domain.member.peresentation;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import team.startup.gwangsan.domain.admin.service.WithDrawnMemberService;
 import team.startup.gwangsan.domain.member.peresentation.dto.request.UpdateMyInfoRequest;
 import team.startup.gwangsan.domain.member.peresentation.dto.response.FindAllUserInfoResponse;
 import team.startup.gwangsan.domain.member.peresentation.dto.response.FindMyInfoResponse;
 import team.startup.gwangsan.domain.member.peresentation.dto.response.FindUserInfoResponse;
-import team.startup.gwangsan.domain.member.service.FindAllUserInfoService;
-import team.startup.gwangsan.domain.member.service.FindMyInfoService;
-import team.startup.gwangsan.domain.member.service.FindUserInfoService;
-import team.startup.gwangsan.domain.member.service.UpdateMyInfoService;
+import team.startup.gwangsan.domain.member.service.*;
 
 import java.util.List;
 
@@ -24,6 +23,7 @@ public class MemberController {
     private final UpdateMyInfoService updateMyInfoService;
     private final FindUserInfoService findUserInfoService;
     private final FindAllUserInfoService findAllUserInfoService;
+    private final MemberWithdrawalService memberWithdrawalService;
 
     @GetMapping
     public ResponseEntity<FindMyInfoResponse> findMyInfo() {
@@ -48,5 +48,10 @@ public class MemberController {
         return ResponseEntity.ok(response);
     }
 
+    @DeleteMapping
+    public ResponseEntity<Void> deleteMember() {
+        memberWithdrawalService.execute();
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 }
 
