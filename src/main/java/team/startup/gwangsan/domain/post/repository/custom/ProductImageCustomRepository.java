aad5b10110a16caacc2ca0789673281fea5bc95a
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-09-19 00:47:06 +0900

    update :: 메서드 추가

--- src/main/java/team/startup/gwangsan/domain/post/repository/custom/ProductImageCustomRepository.java
+++ src/main/java/team/startup/gwangsan/domain/post/repository/custom/ProductImageCustomRepository.java
@@ -6,4 +6,6 @@ import java.util.List;
 
 public interface ProductImageCustomRepository {
     List<ProductImage> findProductImageByProductIdIn(List<Long> productIds);
+
+    List<ProductImage> findAllByProductId(Long productId);
 }
