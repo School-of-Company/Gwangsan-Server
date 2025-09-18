8fef3e56e92aad6587b263eb76d9bd823dfb4620
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-09-19 00:43:18 +0900

    delete :: 불필요한 클래스 삭제

--- src/main/java/team/startup/gwangsan/domain/post/repository/custom/TradeCompleteCustomRepository.java
+++
@@ -1,5 +0,0 @@
-package team.startup.gwangsan.domain.post.repository.custom;
-
-public interface TradeCompleteCustomRepository {
-    long countMutualTradeComplete(Long productId, Long memberId1, Long memberId2);
-}
