0c27eb15dd657d45a58914cff2a4a939ac38b761
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-09-19 00:46:52 +0900

    update :: 메서드 제거

--- src/main/java/team/startup/gwangsan/domain/post/repository/ProductImageRepository.java
+++ src/main/java/team/startup/gwangsan/domain/post/repository/ProductImageRepository.java
@@ -1,16 +1,9 @@
 package team.startup.gwangsan.domain.post.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import team.startup.gwangsan.domain.post.entity.Product;
 import team.startup.gwangsan.domain.post.entity.ProductImage;
 import team.startup.gwangsan.domain.post.repository.custom.ProductImageCustomRepository;
 
-import java.util.List;
-
 public interface ProductImageRepository extends JpaRepository<ProductImage, Long>, ProductImageCustomRepository {
-    List<ProductImage> findAllByProductId(Long productId);
-
     void deleteByProductIdAndImageId(Long productId, Long imageId);
-
-    List<ProductImage> findAllByProduct(Product product);
 }
