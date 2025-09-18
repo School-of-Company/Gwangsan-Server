a7e2d7cabc634ab836904e4ea2fc0484922c5187
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-09-19 00:30:07 +0900

    update :: 메서드 추가

--- src/main/java/team/startup/gwangsan/domain/post/repository/TradeCompleteRepository.java
+++ src/main/java/team/startup/gwangsan/domain/post/repository/TradeCompleteRepository.java
@@ -4,8 +4,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import team.startup.gwangsan.domain.member.entity.Member;
 import team.startup.gwangsan.domain.post.entity.Product;
 import team.startup.gwangsan.domain.post.entity.TradeComplete;
+import team.startup.gwangsan.domain.post.entity.constant.TradeStatus;
 import team.startup.gwangsan.domain.post.repository.custom.TradeCompleteCustomRepository;
 
+import java.util.Optional;
+
 public interface TradeCompleteRepository extends JpaRepository<TradeComplete, Long>, TradeCompleteCustomRepository {
-    boolean existsByProductAndMemberAndOtherMember(Product product, Member member, Member otherMember);
+    boolean existsByProductAndSeller(Product product, Member seller);
+
+    boolean existsByProductAndBuyerAndSellerAndStatus(Product product, Member buyer, Member seller, TradeStatus status);
+
+    Optional<TradeComplete> findByProductAndBuyerAndSellerAndStatus(Product product, Member buyer, Member seller, TradeStatus tradeStatus);
 }
