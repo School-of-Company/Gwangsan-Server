b750dad7a24b4b6767407edf1762120396030601
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-09-20 21:49:27 +0900

    update :: 거래 완료 엔티티 생성 시간 필드 추가

--- src/main/java/team/startup/gwangsan/domain/chat/presentation/dto/GetChatProductDto.java
+++ src/main/java/team/startup/gwangsan/domain/chat/presentation/dto/GetChatProductDto.java
@@ -2,12 +2,14 @@ package team.startup.gwangsan.domain.chat.presentation.dto;
 
 import team.startup.gwangsan.domain.image.presentation.dto.response.GetImageResponse;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public record GetChatProductDto(
         Long id,
         String title,
         List<GetImageResponse> images,
+        LocalDateTime createdAt,
         boolean isSeller,
         boolean isCompletable
 )  {
