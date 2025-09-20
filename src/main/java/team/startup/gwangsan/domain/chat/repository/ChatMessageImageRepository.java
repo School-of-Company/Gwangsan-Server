613ac2c5363a1fca25e164f6dd91930085d02dab
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-09-20 11:50:51 +0900

    update :: 불필요한 메서드 제거 및 상속 추가

--- src/main/java/team/startup/gwangsan/domain/chat/repository/ChatMessageImageRepository.java
+++ src/main/java/team/startup/gwangsan/domain/chat/repository/ChatMessageImageRepository.java
@@ -2,9 +2,7 @@ package team.startup.gwangsan.domain.chat.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import team.startup.gwangsan.domain.chat.entity.ChatMessageImage;
+import team.startup.gwangsan.domain.chat.repository.custom.ChatMessageImageCustomRepository;
 
-import java.util.List;
-
-public interface ChatMessageImageRepository extends JpaRepository<ChatMessageImage, Long> {
-    List<ChatMessageImage> findAllByChatMessageIdIn(List<Long> chatMessageId);
+public interface ChatMessageImageRepository extends JpaRepository<ChatMessageImage, Long>, ChatMessageImageCustomRepository {
 }
