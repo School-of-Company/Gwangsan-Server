bc227a66908d009a67a0be654b4f763d6ce2f040
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-09-20 11:52:13 +0900

    update :: 응답 값 수정

--- src/main/java/team/startup/gwangsan/domain/chat/service/FindChatMessageByRoomIdService.java
+++ src/main/java/team/startup/gwangsan/domain/chat/service/FindChatMessageByRoomIdService.java
@@ -1,10 +1,9 @@
 package team.startup.gwangsan.domain.chat.service;
 
-import team.startup.gwangsan.domain.chat.presentation.dto.response.GetChatMessageResponse;
+import team.startup.gwangsan.domain.chat.presentation.dto.response.GetChatMessagesResponse;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 public interface FindChatMessageByRoomIdService {
-    List<GetChatMessageResponse> execute(Long roomId, LocalDateTime lastCreatedAt, Long lastMessageId, int limit);
+    GetChatMessagesResponse execute(Long roomId, LocalDateTime lastCreatedAt, Long lastMessageId, int limit);
 }
