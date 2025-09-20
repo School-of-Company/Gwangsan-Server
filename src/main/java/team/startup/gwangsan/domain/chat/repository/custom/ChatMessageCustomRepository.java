20e9bcf130fb648ccde8d86e97e99ad575a3b944
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-09-20 11:50:37 +0900

    update :: chatRoom 조회 로직 수정

--- src/main/java/team/startup/gwangsan/domain/chat/repository/custom/ChatMessageCustomRepository.java
+++ src/main/java/team/startup/gwangsan/domain/chat/repository/custom/ChatMessageCustomRepository.java
@@ -6,7 +6,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public interface ChatMessageCustomRepository {
-    List<ChatMessage> findChatMessageByRoomIdsWithCursorPaging(List<Long> roomIds, LocalDateTime lastCreatedAt, Long lastMessageId, int limit);
+    List<ChatMessage> findChatMessageByRoomIdWithCursorPaging(Long roomId, LocalDateTime lastCreatedAt, Long lastMessageId, int limit);
 
     List<ChatMessage> findUnreadMessages(Long roomId, Long lastMessageId, Long readerId);
 }
