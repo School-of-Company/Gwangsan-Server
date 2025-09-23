509745fc5699c068ac7c30eb49ad15550a35855f
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-09-23 13:41:21 +0900

    update :: 자료형 수정

--- src/main/java/team/startup/gwangsan/global/event/SendNotificationEvent.java
+++ src/main/java/team/startup/gwangsan/global/event/SendNotificationEvent.java
@@ -1,11 +1,12 @@
 package team.startup.gwangsan.global.event;
 
+import team.startup.gwangsan.domain.notification.entity.DeviceToken;
 import team.startup.gwangsan.domain.notification.entity.constant.NotificationType;
 
 import java.util.List;
 
 public record SendNotificationEvent(
-        List<String> deviceTokens,
+        List<DeviceToken> deviceTokens,
         NotificationType type,
         Long sourceId
 ) {
