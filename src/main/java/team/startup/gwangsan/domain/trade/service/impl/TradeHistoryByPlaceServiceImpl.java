ef836b757f1886188ae9368b76db79251eb228cf
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-09-23 11:53:57 +0900

    add :: TradeHistoryByPlaceServiceImpl

---
+++ src/main/java/team/startup/gwangsan/domain/trade/service/impl/TradeHistoryByPlaceServiceImpl.java
@@ -0,0 +1,25 @@
+package team.startup.gwangsan.domain.trade.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import team.startup.gwangsan.domain.post.repository.TradeCompleteRepository;
+import team.startup.gwangsan.domain.trade.presentation.dto.request.constant.Period;
+import team.startup.gwangsan.domain.trade.presentation.dto.response.PlaceTradeHistoryResponse;
+import team.startup.gwangsan.domain.trade.service.TradeHistoryByPlaceService;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class TradeHistoryByPlaceServiceImpl implements TradeHistoryByPlaceService {
+
+    private final TradeCompleteRepository tradeCompleteRepository;
+
+    @Override
+    public PlaceTradeHistoryResponse execute(Period period, Integer placeId) {
+        return new PlaceTradeHistoryResponse(
+                tradeCompleteRepository.countByPlaceId(
+                        period.getValue(), LocalDateTime.now(), placeId)
+        );
+    }
+}
