9244079cd96cfd0f952b10218c74b657fc40c9b7
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-09-23 11:54:20 +0900

    update :: TradeCompleteCustomRepository 상속 추가

--- src/main/java/team/startup/gwangsan/domain/post/repository/TradeCompleteRepository.java
+++ src/main/java/team/startup/gwangsan/domain/post/repository/TradeCompleteRepository.java
@@ -5,10 +5,11 @@ import team.startup.gwangsan.domain.member.entity.Member;
 import team.startup.gwangsan.domain.post.entity.Product;
 import team.startup.gwangsan.domain.post.entity.TradeComplete;
 import team.startup.gwangsan.domain.post.entity.constant.TradeStatus;
+import team.startup.gwangsan.domain.post.repository.custom.TradeCompleteCustomRepository;
 
 import java.util.Optional;
 
-public interface TradeCompleteRepository extends JpaRepository<TradeComplete, Long> {
+public interface TradeCompleteRepository extends JpaRepository<TradeComplete, Long>, TradeCompleteCustomRepository {
     Optional<TradeComplete> findByProductAndSeller(Product product, Member seller);
 
     boolean existsByProductAndBuyerAndSellerAndStatus(Product product, Member buyer, Member seller, TradeStatus status);
