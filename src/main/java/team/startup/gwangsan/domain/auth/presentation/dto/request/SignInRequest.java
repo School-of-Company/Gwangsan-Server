54076937432922230f453fdbfa43fa8b1a420c5c
seungchan <s24041@gsm.hs.kr>
2025-09-23 20:22:03 +0900

    update :: auth 인증 간소화

--- src/main/java/team/startup/gwangsan/domain/auth/presentation/dto/request/SignInRequest.java
+++ src/main/java/team/startup/gwangsan/domain/auth/presentation/dto/request/SignInRequest.java
@@ -7,7 +7,10 @@ import team.startup.gwangsan.domain.auth.entity.constant.OsType;
 public record SignInRequest(
 
         @NotBlank
-        @Pattern(regexp = "^[가-힣]+$", message = "닉네임은 한글만 입력 가능합니다.")
+        @Pattern(
+                regexp = "^[가-힣a-zA-Z0-9 ()~]+$",
+                message = "닉네임은 한글, 영문, 숫자, 공백, (), ~ 만 입력 가능합니다."
+        )
         String nickname,
 
         @NotBlank
