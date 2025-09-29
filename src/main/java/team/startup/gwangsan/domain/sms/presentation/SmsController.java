649726e2e60648660143fe8752dc28f5e35c37c4
seungchan <s24041@gsm.hs.kr>
2025-09-29 16:10:51 +0900

    add :: 비밀번호 재설정 인증번호 발송, 인증 추가

--- src/main/java/team/startup/gwangsan/domain/sms/presentation/SmsController.java
+++ src/main/java/team/startup/gwangsan/domain/sms/presentation/SmsController.java
@@ -7,8 +7,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import team.startup.gwangsan.domain.sms.presentation.dto.SendSmsRequest;
 import team.startup.gwangsan.domain.sms.presentation.dto.VerifyCodeRequest;
+import team.startup.gwangsan.domain.sms.service.SendResetPasswordSmsService;
 import team.startup.gwangsan.domain.sms.service.SendSmsService;
 import team.startup.gwangsan.domain.sms.service.VerifyCodeService;
+import team.startup.gwangsan.domain.sms.service.VerifyResetPasswordCodeService;
 
 @RestController
 @RequestMapping("/api/sms")
@@ -17,6 +19,8 @@ public class SmsController {
 
     private final SendSmsService sendSmsService;
     private final VerifyCodeService verifyCodeService;
+    private final SendResetPasswordSmsService sendResetPasswordSmsService;
+    private final VerifyResetPasswordCodeService verifyResetPasswordCodeService;
 
     @PostMapping
     public ResponseEntity<SingleMessageSentResponse> sendSms(@RequestBody @Valid SendSmsRequest request) {
@@ -27,6 +31,18 @@ public class SmsController {
     @PostMapping("/verify")
     public ResponseEntity<Void> verifySms(@RequestBody @Valid VerifyCodeRequest request) {
         verifyCodeService.execute(request);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/password")
+    public ResponseEntity<SingleMessageSentResponse> sendResetPasswordSms(@RequestBody @Valid SendSmsRequest request) {
+        SingleMessageSentResponse response = sendResetPasswordSmsService.execute(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/password/verify")
+    public ResponseEntity<Void> verifyResetPasswordSms(@RequestBody @Valid VerifyCodeRequest request) {
+        verifyResetPasswordCodeService.execute(request);
         return ResponseEntity.ok().build();
     }
 
