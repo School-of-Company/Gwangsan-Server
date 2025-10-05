946d21bcdfce6244b2fe97c5a56a71ad47b409a3
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-10-05 18:53:05 +0900

    update :: AlertReceipt로 수정

--- src/main/java/team/startup/gwangsan/domain/alert/service/impl/ExistsUnreadAlertServiceImpl.java
+++ src/main/java/team/startup/gwangsan/domain/alert/service/impl/ExistsUnreadAlertServiceImpl.java
@@ -2,8 +2,9 @@ package team.startup.gwangsan.domain.alert.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import team.startup.gwangsan.domain.alert.presentation.dto.response.ExistsAlertResponse;
-import team.startup.gwangsan.domain.alert.repository.AlertRepository;
+import team.startup.gwangsan.domain.alert.repository.AlertReceiptRepository;
 import team.startup.gwangsan.domain.alert.service.ExistsUnreadAlertService;
 import team.startup.gwangsan.domain.member.entity.Member;
 import team.startup.gwangsan.global.util.MemberUtil;
@@ -12,13 +13,14 @@ import team.startup.gwangsan.global.util.MemberUtil;
 @RequiredArgsConstructor
 public class ExistsUnreadAlertServiceImpl implements ExistsUnreadAlertService {
 
-    private final AlertRepository alertRepository;
+    private final AlertReceiptRepository alertReceiptRepository;
     private final MemberUtil memberUtil;
 
     @Override
+    @Transactional(readOnly = true)
     public ExistsAlertResponse execute() {
         Member member = memberUtil.getCurrentMember();
 
-        return new ExistsAlertResponse(alertRepository.existsByMemberAndChecked(member, false));
+        return new ExistsAlertResponse(alertReceiptRepository.existsByMemberIdAndChecked(member.getId(), false));
     }
 }
