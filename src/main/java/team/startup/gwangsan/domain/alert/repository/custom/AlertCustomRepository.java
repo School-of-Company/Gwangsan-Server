10eadc058535cba98bfce19d452c7215abf9359b
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-10-05 18:19:20 +0900

    delete :: 불필요한 클래스 제거

--- src/main/java/team/startup/gwangsan/domain/alert/repository/custom/AlertCustomRepository.java
+++
@@ -1,9 +0,0 @@
- package team.startup.gwangsan.domain.alert.repository.custom;
-
-import team.startup.gwangsan.domain.alert.entity.Alert;
-
-import java.util.List;
-
-public interface AlertCustomRepository {
-    List<Alert> findUnreadAlerts(Long alertId, Long memberId);
-}
