b78b8f0b73f5644e85c7459a4265799f4a925827
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-10-18 16:37:53 +0900

    update :: 커스텀 레포지토리 상속 추가

--- src/main/java/team/startup/gwangsan/domain/member/repository/MemberRepository.java
+++ src/main/java/team/startup/gwangsan/domain/member/repository/MemberRepository.java
@@ -3,11 +3,12 @@ package team.startup.gwangsan.domain.member.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import team.startup.gwangsan.domain.member.entity.Member;
 import team.startup.gwangsan.domain.member.entity.constant.MemberStatus;
+import team.startup.gwangsan.domain.member.repository.custom.MemberCustomRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberCustomRepository {
     Optional<Member> findByPhoneNumber(String phoneNumber);
 
     boolean existsByPhoneNumber(String phoneNumber);
