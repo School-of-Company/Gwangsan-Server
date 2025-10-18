bce8a743639823d6f3f2ceb8c02a8573b3a2ac4c
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-10-18 14:17:14 +0900

    delete :: 불필요한 서비스 클래스 제거

--- src/main/java/team/startup/gwangsan/domain/admin/service/CreateTradeCompleteAlertService.java
+++
@@ -1,7 +0,0 @@
-package team.startup.gwangsan.domain.admin.service;
-
-import team.startup.gwangsan.domain.admin.entity.constant.AlertType;
-
-public interface CreateTradeCompleteAlertService {
-    void execute(AlertType type, Long sourceId, Long otherMemberId, Long requesterMemberId);
-}
