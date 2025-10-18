88cd096137c65db2dfe05bee1305a7f8858d4fe9
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-10-18 15:31:11 +0900

    update :: 신고 처리 완료 시 알림 발송을 위한 필드 추가

--- src/main/java/team/startup/gwangsan/global/event/CreateAlertEvent.java
+++ src/main/java/team/startup/gwangsan/global/event/CreateAlertEvent.java
@@ -5,6 +5,10 @@ import team.startup.gwangsan.domain.alert.entity.constant.AlertType;
 public record CreateAlertEvent(
         Long sourceId,
         Long memberId,
-        AlertType alertType
+        AlertType alertType,
+        Long suspendId
 ) {
+    public CreateAlertEvent(Long sourceId, Long memberId, AlertType alertType) {
+        this(sourceId, memberId, alertType, null);
+    }
 }
