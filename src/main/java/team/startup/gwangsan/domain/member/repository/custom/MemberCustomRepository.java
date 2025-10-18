d3c21000e9d5765aef9b1efa90384a563560547a
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-10-18 16:37:37 +0900

    update :: findProductMemberResponsesByMemberIds 메서드 추가

--- src/main/java/team/startup/gwangsan/domain/member/repository/custom/MemberCustomRepository.java
+++ src/main/java/team/startup/gwangsan/domain/member/repository/custom/MemberCustomRepository.java
@@ -1,11 +1,10 @@
 package team.startup.gwangsan.domain.member.repository.custom;
 
-import team.startup.gwangsan.domain.member.entity.Member;
-import team.startup.gwangsan.domain.member.entity.constant.MemberStatus;
-import team.startup.gwangsan.domain.place.entity.Place;
+import team.startup.gwangsan.domain.post.presentation.dto.response.GetProductMemberResponse;
 
+import java.util.Collection;
 import java.util.List;
 
 public interface MemberCustomRepository {
-    List<Member> findByStatusAndPlaces(MemberStatus status, List<Place> places);
+    List<GetProductMemberResponse> findProductMemberResponsesByMemberIds(Collection<Long> memberIds);
 }
