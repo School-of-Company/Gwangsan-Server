66a32f4c1790171115da1bb1bc481dff669b9c4f
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-10-18 16:38:28 +0900

    update :: 불필요한 로직 제거 및 거래 철회 거절 로직 추가

--- src/main/java/team/startup/gwangsan/domain/admin/service/impl/RejectAdminAlertServiceImpl.java
+++ src/main/java/team/startup/gwangsan/domain/admin/service/impl/RejectAdminAlertServiceImpl.java
@@ -1,6 +1,7 @@
 package team.startup.gwangsan.domain.admin.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import team.startup.gwangsan.domain.admin.entity.AdminAlert;
@@ -8,32 +9,32 @@ import team.startup.gwangsan.domain.admin.exception.NotFoundAdminAlertException;
 import team.startup.gwangsan.domain.admin.repository.AdminAlertRepository;
 import team.startup.gwangsan.domain.admin.service.RejectAdminAlertService;
 import team.startup.gwangsan.domain.admin.util.ValidatePlaceUtil;
+import team.startup.gwangsan.domain.alert.entity.constant.AlertType;
 import team.startup.gwangsan.domain.member.entity.Member;
 import team.startup.gwangsan.domain.member.entity.MemberDetail;
 import team.startup.gwangsan.domain.member.exception.NotFoundMemberDetailException;
-import team.startup.gwangsan.domain.member.exception.NotFoundMemberException;
 import team.startup.gwangsan.domain.member.repository.MemberDetailRepository;
-import team.startup.gwangsan.domain.member.repository.MemberRepository;
-import team.startup.gwangsan.domain.post.entity.TradeComplete;
-import team.startup.gwangsan.domain.post.repository.TradeCompleteRepository;
 import team.startup.gwangsan.domain.report.entity.Report;
 import team.startup.gwangsan.domain.report.exception.NotFoundReportException;
 import team.startup.gwangsan.domain.report.repository.ReportRepository;
+import team.startup.gwangsan.domain.trade.entity.TradeCancel;
+import team.startup.gwangsan.domain.trade.entity.constant.TradeCancelStatus;
+import team.startup.gwangsan.domain.trade.exception.NotFoundTradeCancelException;
+import team.startup.gwangsan.domain.trade.repository.TradeCancelRepository;
+import team.startup.gwangsan.global.event.CreateAlertEvent;
 import team.startup.gwangsan.global.util.MemberUtil;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class RejectAdminAlertServiceImpl implements RejectAdminAlertService {
 
     private final AdminAlertRepository adminAlertRepository;
-    private final MemberRepository memberRepository;
-    private final TradeCompleteRepository tradeCompleteRepository;
     private final ReportRepository reportRepository;
     private final MemberUtil memberUtil;
     private final MemberDetailRepository memberDetailRepository;
     private final ValidatePlaceUtil validatePlaceUtil;
+    private final TradeCancelRepository tradeCancelRepository;
+    private final ApplicationEventPublisher applicationEventPublisher;
 
     @Override
     @Transactional
@@ -50,8 +51,8 @@ public class RejectAdminAlertServiceImpl implements RejectAdminAlertService {
 
         switch (alert.getType()) {
             case SIGN_UP -> rejectSignUp(alertMemberDetail);
-            case TRADE_COMPLETE -> rejectTradeComplete(alert);
             case REPORT -> rejectReport(alert);
+            case TRADE_CANCEL -> rejectTradeCancel(alert);
         }
     }
 
@@ -69,19 +70,27 @@ public class RejectAdminAlertServiceImpl implements RejectAdminAlertService {
         memberDetailRepository.delete(memberDetail);
     }
 
-    private void rejectTradeComplete(AdminAlert alert) {
-        List<Long> memberIds = List.of(
-                alert.getOtherMember().getId(),
-                alert.getRequester().getId()
-        );
-        List<TradeComplete> tradeCompletes = tradeCompleteRepository.findAllById(memberIds);
-        tradeCompleteRepository.deleteAll(tradeCompletes);
-    }
-
     private void rejectReport(AdminAlert alert) {
         Report report = reportRepository.findById(alert.getSourceId())
                 .orElseThrow(NotFoundReportException::new);
-        reportRepository.delete(report);
+
+        applicationEventPublisher.publishEvent(new CreateAlertEvent(
+                report.getId(),
+                report.getReporter().getId(),
+                AlertType.REPORT_REJECT
+        ));
     }
 
+    private void rejectTradeCancel(AdminAlert alert) {
+        TradeCancel tradeCancel = tradeCancelRepository.findById(alert.getSourceId())
+                .orElseThrow(NotFoundTradeCancelException::new);
+
+        tradeCancel.updateStatus(TradeCancelStatus.REJECTED);
+
+        applicationEventPublisher.publishEvent(new CreateAlertEvent(
+                tradeCancel.getId(),
+                tradeCancel.getMember().getId(),
+                AlertType.TRADE_CANCEL_REJECT
+        ));
+    }
 }
