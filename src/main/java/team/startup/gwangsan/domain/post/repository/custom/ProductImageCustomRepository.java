80edcfbb458d5d5098cfa160f43c12fca5192ebc
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-10-18 16:38:09 +0900

    update :: 메서드 명 수정

--- src/main/java/team/startup/gwangsan/domain/post/repository/custom/ProductImageCustomRepository.java
+++ src/main/java/team/startup/gwangsan/domain/post/repository/custom/ProductImageCustomRepository.java
@@ -2,10 +2,11 @@ package team.startup.gwangsan.domain.post.repository.custom;
 
 import team.startup.gwangsan.domain.post.entity.ProductImage;
 
+import java.util.Collection;
 import java.util.List;
 
 public interface ProductImageCustomRepository {
-    List<ProductImage> findProductImageByProductIdIn(List<Long> productIds);
+    List<ProductImage> findAllByProductIdIn(Collection<Long> productIds);
 
     List<ProductImage> findAllByProductId(Long productId);
 }
