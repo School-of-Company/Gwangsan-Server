e4bbbc0b7eb4f760431199f7c281712a5a2c2a45
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-10-18 15:31:11 +0900

    update :: 신고 처리 완료 시 알림 발송을 위한 필드 추가

--- src/main/java/team/startup/gwangsan/domain/alert/service/CreateAlertService.java
+++ src/main/java/team/startup/gwangsan/domain/alert/service/CreateAlertService.java
@@ -3,5 +3,5 @@ package team.startup.gwangsan.domain.alert.service;
 import team.startup.gwangsan.domain.alert.entity.constant.AlertType;
 
 public interface CreateAlertService {
-    void execute(Long sourceId, Long memberId, AlertType alertType);
+    void execute(Long sourceId, Long memberId, AlertType alertType, Long suspendId);
 }
