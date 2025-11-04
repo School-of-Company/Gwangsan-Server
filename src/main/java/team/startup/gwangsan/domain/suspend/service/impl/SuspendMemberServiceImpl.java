d527909b833254920467f2724bcd077bc4e2e014
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-11-04 17:50:36 +0900

    update :: alertId 추가

--- src/main/java/team/startup/gwangsan/domain/suspend/service/impl/SuspendMemberServiceImpl.java
+++ src/main/java/team/startup/gwangsan/domain/suspend/service/impl/SuspendMemberServiceImpl.java
@@ -1,8 +1,11 @@
 package team.startup.gwangsan.domain.suspend.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import team.startup.gwangsan.domain.admin.repository.AdminAlertRepository;
+import team.startup.gwangsan.domain.alert.entity.constant.AlertType;
 import team.startup.gwangsan.domain.member.entity.Member;
 import team.startup.gwangsan.domain.member.entity.constant.MemberStatus;
 import team.startup.gwangsan.domain.member.exception.NotFoundMemberException;
@@ -10,6 +13,7 @@ import team.startup.gwangsan.domain.member.repository.MemberRepository;
 import team.startup.gwangsan.domain.suspend.entity.Suspend;
 import team.startup.gwangsan.domain.suspend.repository.SuspendRepository;
 import team.startup.gwangsan.domain.suspend.service.SuspendMemberService;
+import team.startup.gwangsan.global.event.CreateAlertEvent;
 
 import java.time.LocalDateTime;
 
@@ -19,10 +23,12 @@ public class SuspendMemberServiceImpl implements SuspendMemberService {
 
     private final MemberRepository memberRepository;
     private final SuspendRepository suspendRepository;
+    private final AdminAlertRepository adminAlertRepository;
+    private final ApplicationEventPublisher applicationEventPublisher;
 
     @Override
     @Transactional
-    public void execute(Long memberId, int suspendedDays) {
+    public void execute(Long memberId, int suspendedDays, Long alertId) {
         Member member = memberRepository.findById(memberId)
                 .orElseThrow(NotFoundMemberException::new);
 
@@ -36,8 +42,17 @@ public class SuspendMemberServiceImpl implements SuspendMemberService {
                 .suspendedUntil(until)
                 .build();
 
-        suspendRepository.save(suspend);
+        suspend = suspendRepository.save(suspend);
 
         member.updateMemberStatus(MemberStatus.SUSPENDED);
+
+        if (alertId != null && !adminAlertRepository.existsById(alertId)) {
+            applicationEventPublisher.publishEvent(new CreateAlertEvent(
+                    alertId,
+                    memberId,
+                    AlertType.REPORT,
+                    suspend.getId()
+            ));
+        }
     }
 }
