c0cacb253a4222deed48516dcdebbf94eeca1af6
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-11-12 23:13:22 +0900

    update :: 사용하지 않는 이미지 조회 메서드 추가

--- src/main/java/team/startup/gwangsan/domain/image/repository/ImageRepository.java
+++ src/main/java/team/startup/gwangsan/domain/image/repository/ImageRepository.java
@@ -2,9 +2,10 @@ package team.startup.gwangsan.domain.image.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import team.startup.gwangsan.domain.image.entity.Image;
+import team.startup.gwangsan.domain.image.repository.custom.ImageCustomRepository;
 
 import java.util.List;
 
-public interface ImageRepository extends JpaRepository<Image, Long> {
+public interface ImageRepository extends JpaRepository<Image, Long>, ImageCustomRepository {
     List<Image> findByIdIn(List<Long> imageIds);
 }
