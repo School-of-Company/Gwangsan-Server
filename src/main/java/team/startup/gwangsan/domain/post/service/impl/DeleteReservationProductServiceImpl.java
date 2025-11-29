f6543fd131a4d8dbbb93f61a186f084222115489
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-11-29 14:37:11 +0900

    add :: 거래 예약 관련 로직 수정

--- src/main/java/team/startup/gwangsan/domain/post/service/impl/DeleteReservationProductServiceImpl.java
+++ src/main/java/team/startup/gwangsan/domain/post/service/impl/DeleteReservationProductServiceImpl.java
@@ -3,28 +3,37 @@ package team.startup.gwangsan.domain.post.service.impl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import team.startup.gwangsan.domain.member.entity.Member;
 import team.startup.gwangsan.domain.post.entity.Product;
+import team.startup.gwangsan.domain.post.entity.ProductReservation;
 import team.startup.gwangsan.domain.post.entity.constant.ProductStatus;
-import team.startup.gwangsan.domain.post.exception.NotFoundProductException;
-import team.startup.gwangsan.domain.post.exception.ProductNotReservationException;
+import team.startup.gwangsan.domain.post.entity.constant.ReservationStatus;
+import team.startup.gwangsan.domain.post.exception.ReservationParticipantOnlyException;
 import team.startup.gwangsan.domain.post.repository.ProductRepository;
+import team.startup.gwangsan.domain.post.repository.ProductReservationRepository;
 import team.startup.gwangsan.domain.post.service.DeleteReservationProductService;
+import team.startup.gwangsan.global.util.MemberUtil;
 
 @Service
 @RequiredArgsConstructor
 public class DeleteReservationProductServiceImpl implements DeleteReservationProductService {
 
     private final ProductRepository productRepository;
+    private final MemberUtil memberUtil;
+    private final ProductReservationRepository productReservationRepository;
 
     @Override
     @Transactional
     public void execute(Long productId) {
-        Product product = productRepository.findById(productId)
-                .orElseThrow(NotFoundProductException::new);
+        Member member = memberUtil.getCurrentMember();
 
-        if (product.getStatus() != ProductStatus.RESERVATION) {
-            throw new ProductNotReservationException();
-        }
+        ProductReservation productReservation = productReservationRepository
+                .findByProduct_MemberOrReserverAndStatus(member, member, ReservationStatus.PENDING)
+                .orElseThrow(ReservationParticipantOnlyException::new);
+
+        Product product = productReservation.getProduct();
+
+        productReservation.cancel();
 
         product.updateStatus(ProductStatus.ONGOING);
     }
