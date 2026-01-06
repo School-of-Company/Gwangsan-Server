fcc25ee613548383bd26d13814b609812dce1ac2
seungchan <s24041@gsm.hs.kr>
2026-01-06 23:11:59 +0900

    add :: 필요 메서드 추가

--- src/main/java/team/startup/gwangsan/domain/review/repository/custom/ReviewCustomRepository.java
+++ src/main/java/team/startup/gwangsan/domain/review/repository/custom/ReviewCustomRepository.java
@@ -1,10 +1,9 @@
 package team.startup.gwangsan.domain.review.repository.custom;
 
-import team.startup.gwangsan.domain.member.entity.Member;
-import team.startup.gwangsan.domain.review.entity.Review;
-
 import java.util.List;
 
 public interface ReviewCustomRepository {
-    List<Review> findAllByReviewedWithFetch(Member reviewed);
+    List<MyReviewRow> findMyReviews(Long reviewerId);
+    List<ReceivedReviewRow> findReceivedReviews(Long reviewedId);
+
 }
