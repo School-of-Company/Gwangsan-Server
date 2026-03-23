eeafc1b57bcbb413e3eb42dde11146703b7040ce
suuuuuuminnnnnn <sumin45402214@gmail.com>
2026-03-23 14:59:28 +0900

    update :: 채팅 서버에서 넘겨주는 값 맞춰서 파라미터 수정

--- src/main/java/team/startup/gwangsan/domain/chat/service/SaveChatMessageService.java
+++ src/main/java/team/startup/gwangsan/domain/chat/service/SaveChatMessageService.java
@@ -3,8 +3,9 @@ package team.startup.gwangsan.domain.chat.service;
 import team.startup.gwangsan.domain.chat.entity.constant.MessageType;
 import team.startup.gwangsan.domain.chat.presentation.dto.response.SaveChatMessageResponse;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface SaveChatMessageService {
-    SaveChatMessageResponse execute(Long roomId, String content, List<Long> imageIds, MessageType messageType);
+    SaveChatMessageResponse execute(Long messageId, Long roomId, String content, List<Long> imageIds, MessageType messageType, Long senderId, LocalDateTime createdAt);
 }
