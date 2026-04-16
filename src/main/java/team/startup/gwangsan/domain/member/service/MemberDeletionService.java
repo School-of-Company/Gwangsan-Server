e3f50df360f41af50a6b857157fe4a79e030736e
hamin <hamin090403@gmail.com>
2026-04-16 18:16:13 +0900

    add :: MemberDeletionService 구현

---
+++ src/main/java/team/startup/gwangsan/domain/member/service/MemberDeletionService.java
@@ -0,0 +1,7 @@
+package team.startup.gwangsan.domain.member.service;
+
+import team.startup.gwangsan.domain.member.entity.Member;
+
+public interface MemberDeletionService {
+    void delete(Member member);
+}
